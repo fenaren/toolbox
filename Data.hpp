88a2391f64fd17b60ce4c4910a974023cef3406a
Leigh Garbs <leighgarbs@gmail.com>
2018-05-04 10:26:03 -0600

    A good first hack at a base Data class

---
+++ Data.hpp
@@ -0,0 +1,69 @@
+#if !defined DATA_HPP
+#define DATA_HPP
+
+// Represents an amount of data.
+class Data
+{
+public:
+
+    Data(char* data, unsigned int length);
+
+    // Does nothing
+    ~Data();
+
+    void set(char* data, unsigned int length);
+
+    void setData(char* data);
+
+    void setLength(unsigned int length);
+
+    void get(char*& data, unsigned int& length) const;
+
+    char* getData() const;
+
+    unsigned int getLength() const;
+
+    bool operator==(Data& data);
+
+    bool operator!=(Data& data);
+
+private:
+
+    char* data;
+
+    unsigned int length;
+};
+
+inline void Data::set(char* data, unsigned int length)
+{
+    this->data   = data;
+    this->length = length;
+}
+
+inline void Data::setData(char* data)
+{
+    this->data = data;
+}
+
+inline void Data::setLength(unsigned int length)
+{
+    this->length = length;
+}
+
+inline void Data::get(char*& data, unsigned int& length) const
+{
+    data   = this->data;
+    length = this->length;
+}
+
+inline char* Data::getData() const
+{
+    return data;
+}
+
+inline unsigned int Data::getLength() const
+{
+    return length;
+}
+
+#endif
