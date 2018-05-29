a5f26bf1e32210dad856867a5188cd55e6e96c82
Leigh Garbs <leighgarbs@gmail.com>
2018-05-28 21:09:54 -0400

    Adding unit test, not done yet

---
+++ unittests/PosixTimespec.ut.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+#include "PosixTimespec.hpp"
+
+int main(int argc, char** argv)
+{
+    std::vector<timespec> timespecs;
+
+    timespec tp;
+
+    tp.tv_sec = 0;
+    tp.tv_nsec = 0;
+    timespecs.push_back(tp);
+
+    tp.tv_sec = 1;
+    tp.tv_nsec = 1;
+    timespecs.push_back(tp);
+
+    tp.tv_sec = 857693847;
+    tp.tv_nsec = 736486;
+    timespecs.push_back(tp);
+
+    tp.tv_sec = 85769;
+    tp.tv_nsec = 566486;
+    timespecs.push_back(tp);
+
+    std::vector<std::pair<unsigned int, unsigned int> > failed_cases;
+
+    for (unsigned int i = 0; i < timespecs.size(); i++)
+    {
+        PosixTimespec ts1(timespecs[i]);
+
+        for (unsigned int j = 0; j < timespecs.size(); j++)
+        {
+            PosixTimespec ts2(timespecs[j]);
+
+            if (i == j)
+            {
+                if (!(ts1 == ts2 && ts2 == ts1))
+                {
+                    failed_cases.push_back(
+                        std::pair<unsigned int, unsigned int>(i, j));
+                }
+            }
+            else
+            {
+                if (!(ts1 != ts2 && ts2 != ts1))
+                {
+                    failed_cases.push_back(
+                        std::pair<unsigned int, unsigned int>(i, j));
+                }
+            }
+        }
+    }
+
+    std::cout << "Failed cases: " << failed_cases.size() << "\n";
+
+    // This unit test passes if no failed cases were recorded; remember that a
+    // zero return value means success
+    return failed_cases.size() != 0;
+}
