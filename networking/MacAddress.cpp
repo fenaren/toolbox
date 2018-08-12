76a1923d9cb2cf7e35250ab028fc3a4d458ddb14
Leigh Garbs <leighgarbs@gmail.com>
2018-08-12 18:31:26 -0400

    WIP

--- networking/MacAddress.cpp
+++ networking/MacAddress.cpp
@@ -68,7 +68,38 @@ MacAddress& MacAddress::operator=(const std::string& mac_address_str)
 //==============================================================================
 bool MacAddress::getHostMacAddresses(std::list<MacAddress>& host_mac_addresses)
 {
+    host_mac_addresses.clear();
+
+#if defined LINUX
+
+    // Getting MAC and IP addresses requires a socket, doesn't matter what kind
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock_fd == -1)
+    {
+        // If something goes wrong, print an error message and quit
+        perror(0);
+        shutdown();
+    }
+
+    // Fill out an ifreq with name of the target interface
+    ifreq iface;
+    strcpy(iface.ifr_name, interface_name.c_str());
+
+    // Request our MAC address
+    if (ioctl(sock_fd, SIOCGIFHWADDR, &iface) == -1)
+    {
+        // If something goes wrong, print an error message and quit
+        perror(0);
+        shutdown();
+    }
+
+    // Initialize own_mac
+    memcpy(own_mac, iface.ifr_hwaddr.sa_data, 6);
+
+#elif defined MACOS
+#else
     return false;
+#endif
 }
 
 //==============================================================================
