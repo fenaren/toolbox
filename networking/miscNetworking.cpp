277d17a0d745b6b7534e3e41e028bc8c750f44d0
Leigh Garbs <leighgarbs@gmail.com>
2018-08-19 15:40:38 -0400

    WIP

--- networking/miscNetworking.cpp
+++ networking/miscNetworking.cpp
@@ -46,20 +46,32 @@ bool miscNetworking::getMacAddress(const std::string& interface_name,
 }
 
 //==============================================================================
-// Retrieves an IP address corresponding to the given interface name
+// Retrieves an IPv4 address corresponding to the given interface name
 //==============================================================================
 bool miscNetworking::getIpv4Address(const std::string& interface_name,
                                     Ipv4Address&       ipv4_address)
 {
 #if defined LINUX
+    // Getting MAC and IP addresses requires a socket, doesn't matter what kind
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock_fd == -1)
+    {
+        return false;
+    }
+
+    // Fill out an ifreq with name of the target interface
+    ifreq iface;
+    strcpy(iface.ifr_name, interface_name.c_str());
+
     // Request our IP address
     if (ioctl(sock_fd, SIOCGIFADDR, &iface) == -1)
     {
+        return false;
     }
 
     // Initialize own IP address
     sockaddr_in* temp_addr = (sockaddr_in*)&iface.ifr_addr;
-    memcpy(own_ip, (const void*)(&(temp_addr->sin_addr.s_addr)), 4);
+    ipv4_address.readRaw(&temp_addr->sin_addr.s_addr);
 
     return true;
 #else
