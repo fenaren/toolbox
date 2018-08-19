143d0a5ff7f92d2bbf942ec0fefe58480e702498
Leigh Garbs <leighgarbs@gmail.com>
2018-08-19 14:49:20 -0400

    WIP

--- networking/miscNetworking.cpp
+++ networking/miscNetworking.cpp
@@ -1,55 +1,60 @@
 #include "miscNetworking.hpp"
 
+#include <netinet/in.h>
+#include <sys/socket.h>
+
 #include "Ipv4Address.hpp"
 #include "MacAddress.hpp"
 
-namespace miscNetworking
+//==============================================================================
+// Retrieves the MAC address corresponding to the given interface name
+//==============================================================================
+bool miscNetworking::getMacAddress(const std::string& interface_name,
+                                   MacAddress&        mac_address)
 {
-    bool getMacAddress(const std::string& interface_name,
-                       MacAddress&        mac_address)
+#if defined LINUX
+    // Getting MAC and IP addresses requires a socket, doesn't matter what kind
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock_fd == -1)
     {
-        // Getting MAC and IP addresses requires a socket, doesn't matter what
-        // kind
-        int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-        if (sock_fd == -1)
-        {
-            // If something goes wrong, print an error message and quit
-            perror(0);
-            shutdown();
-        }
-
-        // Fill out an ifreq with name of the target interface
-        ifreq iface;
-        strcpy(iface.ifr_name, interface_name.c_str());
-
-        // Request our MAC address
-        if (ioctl(sock_fd, SIOCGIFHWADDR, &iface) == -1)
-        {
-            // If something goes wrong, print an error message and quit
-            perror(0);
-            shutdown();
-        }
-
-        // Initialize own_mac
-        memcpy(own_mac, iface.ifr_hwaddr.sa_data, 6);
     }
 
-    bool getIpv4Address(const std::string& interface_name,
-                        Ipv4Address&       ipv4_address)
+    // Fill out an ifreq with name of the target interface
+    ifreq iface;
+    strcpy(iface.ifr_name, interface_name.c_str());
+
+    // Request our MAC address
+    if (ioctl(sock_fd, SIOCGIFHWADDR, &iface) == -1)
     {
-        // Request our IP address
-        if (ioctl(sock_fd, SIOCGIFADDR, &iface) == -1)
-        {
-            // If something goes wrong, print an error message and quit
-            perror(0);
-            shutdown();
-        }
-
-        // Initialize own IP address
-        sockaddr_in* temp_addr = (sockaddr_in*)&iface.ifr_addr;
-        memcpy(own_ip, (const void*)(&(temp_addr->sin_addr.s_addr)), 4);
-
-        // Not needed anymore; return code not useful enough to bother with
-        close(sock_fd);
     }
+
+    // Initialize own_mac
+    memcpy(own_mac, iface.ifr_hwaddr.sa_data, 6);
+
+    return true;
+#else
+    return false;
+#endif
+}
+
+//==============================================================================
+// Retrieves an IP address corresponding to the given interface name
+//==============================================================================
+bool miscNetworking::getIpv4Address(const std::string& interface_name,
+                                    Ipv4Address&       ipv4_address)
+{
+#if defined LINUX
+    // Request our IP address
+    if (ioctl(sock_fd, SIOCGIFADDR, &iface) == -1)
+    {
+    }
+
+    // Initialize own IP address
+    sockaddr_in* temp_addr = (sockaddr_in*)&iface.ifr_addr;
+    memcpy(own_ip, (const void*)(&(temp_addr->sin_addr.s_addr)), 4);
+
+    return true;
+#else
+    return false;
+#endif
 }
--- networking/miscNetworking.hpp
+++ networking/miscNetworking.hpp
@@ -1,11 +1,20 @@
+#if !defined MISC_NETWORKING_HPP
+#define MISC_NETWORKING_HPP
+
+#include <string>
+
 #include "Ipv4Address.hpp"
 #include "MacAddress.hpp"
 
 namespace miscNetworking
 {
+    // Retrieves the MAC address corresponding to the given interface name
     bool getMacAddress(const std::string& interface_name,
                        MacAddress&        mac_address);
 
+    // Retrieves an IP address corresponding to the given interface name
     bool getIpv4Address(const std::string& interface_name,
                         Ipv4Address&       ipv4_address);
 }
+
+#endif
