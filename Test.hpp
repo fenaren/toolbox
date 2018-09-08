4b51fa1cefe0f13ba6bce86d950d6fb660ebd700
Leigh Garbs <leighgarbs@gmail.com>
2018-09-07 22:14:24 -0400

    WIP

---
+++ Test.hpp
@@ -0,0 +1,20 @@
+#if !defined TEST_HPP
+#define TEST_HPP
+
+#include "Program.hpp"
+
+class Test : public Program
+{
+public:
+
+    Test(int argc, char** argv);
+
+    virtual ~Test();
+
+protected:
+
+private:
+
+};
+
+#endif
