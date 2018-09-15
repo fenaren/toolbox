c19fba3ce56070bb126044d6037d90118668e1bf
Leigh Garbs <leighgarbs@gmail.com>
2018-09-14 21:18:13 -0400

    WIP

--- tests/LogTest.cpp
+++ tests/LogTest.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
+#include "LogTest.hpp"
+
 #include "Log.hpp"
-#include "LogUT.hpp"
 #include "Test.hpp"
 
 //==============================================================================
-LogUT::LogUT(int argc, char** argv) :
-    Program(argc, argv)
+LogTest::LogTest()
 {
 }
 
 //==============================================================================
-LogUT::~LogUT()
+LogTest::~LogTest()
 {
 }
 
 //==============================================================================
-int LogUT::run()
+Test::Result LogTest::run()
 {
     Log log;
     log.write("message1");
@@ -29,11 +29,4 @@ int LogUT::run()
     log.writeError("message3");
 
     return Test::PASSED;
-}
-
-//==============================================================================
-int main(int argc, char** argv)
-{
-    LogUT log_ut(argc, argv);
-    return log_ut.run();
 }
---
+++ tests/LogTest.hpp
@@ -0,0 +1,18 @@
+#ifndef LOG_TEST_HPP
+#define LOG_TEST_HPP
+
+#include "Test.hpp"
+
+class LogTest : public Test
+{
+public:
+
+    LogTest();
+
+    ~LogTest();
+
+    // Implements the test
+    virtual Test::Result run();
+};
+
+#endif
