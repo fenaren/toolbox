2fdde073546f48ece3af6440e7e8d373ee1a9ba5
Leigh Garbs <leighgarbs@gmail.com>
2018-09-25 21:51:07 -0400

    This should work

--- tests/Ipv4Address_test.cpp
+++ tests/Ipv4Address_test.cpp
@@ -56,7 +56,6 @@ Test::Result Ipv4Address_test::run()
                       ipv4_address_i == unique_ipv4_addresses[j] &&
                       // cppcheck-suppress duplicateExpression
                       ipv4_address_j == unique_ipv4_addresses[i] &&
-                      // cppcheck-suppress duplicateExpression
                       unique_ipv4_addresses[i] == ipv4_address_j &&
                       unique_ipv4_addresses[j] == ipv4_address_i))
                 {
@@ -67,7 +66,9 @@ Test::Result Ipv4Address_test::run()
             else
             {
                 if (!(ipv4_address_i != ipv4_address_j &&
+                      // cppcheck-suppress duplicateExpression
                       ipv4_address_i != unique_ipv4_addresses[j] &&
+                      // cppcheck-suppress duplicateExpression
                       ipv4_address_j != unique_ipv4_addresses[i] &&
                       unique_ipv4_addresses[i] != ipv4_address_j &&
                       unique_ipv4_addresses[j] != ipv4_address_i))
