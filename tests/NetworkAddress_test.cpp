a3edfc18278693379a4a3ebbd72c011aee40bca1
Leigh Garbs <leighgarbs@gmail.com>
2018-09-30 17:24:24 -0400

    NetworkAddress unit test should be done

--- tests/NetworkAddress_test.cpp
+++ tests/NetworkAddress_test.cpp
@@ -35,9 +35,10 @@ Test::Result NetworkAddress_test::run()
 {
     // Makes no sense to create an network address of 0 bytes but we should test
     // it here
-    NetworkAddress test_na1(0);         // Dynamic allocation
-    NetworkAddress test_na2(10);        // Dynamic allocation
-    NetworkAddress test_na3(test_na2);  // Tests the copy constructor
+    unsigned int test_na2_len = 10;
+    NetworkAddress test_na1(0);            // Dynamic allocation
+    NetworkAddress test_na2(test_na2_len); // Dynamic allocation
+    NetworkAddress test_na3(test_na2);     // Tests the copy constructor
 
     unsigned int test_data_len = 20;
     unsigned char test_data[test_data_len];
@@ -55,7 +56,7 @@ Test::Result NetworkAddress_test::run()
     test_na4.writeRaw(test_data2);
     MUST_BE_TRUE(memcmp(test_data, test_data2, test_data_len) == 0);
 
-    // Tests equality and inequality
+    // Tests readRaw, equality, inequality
     NetworkAddress test_na5(test_data_len);
     test_na5.readRaw(test_data2);
     MUST_BE_TRUE(test_na4 == test_na5);
@@ -69,6 +70,13 @@ Test::Result NetworkAddress_test::run()
         test_na4.setOctet(i, 0);
         MUST_BE_TRUE(test_data[i] == 0);
     }
+
+    // Tests assignment
+    memset(test_data,  0, test_data_len);
+    memset(test_data2, 1, test_data_len);
+    NetworkAddress test_na6(test_data2, test_data_len);
+    test_na4 = test_na6;
+    MUST_BE_TRUE(memcmp(test_data, test_data2, test_data_len) == 0);
 
     return Test::PASSED;
 }
