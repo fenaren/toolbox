75f84505dd4ac0718de5fa727c2267c091382a0f
Leigh Garbs <leighgarbs@gmail.com>
2018-09-30 13:26:02 -0400

    Forgot these

---
+++ networking/NetworkAddress.cpp
@@ -0,0 +1,94 @@
+#include <cstring>
+#include <new>
+
+#include "NetworkAddress.hpp"
+
+#include "Field.hpp"
+
+//==============================================================================
+// Dynamically allocates an address that is "length_bytes" in size
+//==============================================================================
+NetworkAddress::NetworkAddress(unsigned int length_bytes) :
+    Field(),
+    length_bytes(length_bytes),
+    network_address_raw_owned(true)
+{
+    network_address_raw = new unsigned char[length_bytes];
+    memset(network_address_raw, 0, length_bytes);
+}
+
+//==============================================================================
+// Uses the memory at "buffer" (of size "length_bytes") as the raw network
+// address; does not dynamically allocate memory
+//==============================================================================
+NetworkAddress::NetworkAddress(unsigned char* buffer,
+                               unsigned int   length_bytes) :
+    Field(),
+    network_address_raw(buffer),
+    length_bytes(length_bytes),
+    network_address_raw_owned(false)
+{
+}
+
+//==============================================================================
+// Frees the memory at "network_address_raw" if owned by this class
+//==============================================================================
+NetworkAddress::~NetworkAddress()
+{
+    if (network_address_raw_owned)
+    {
+        delete[] network_address_raw;
+    }
+}
+
+//==============================================================================
+// Reads the field from the "buffer" memory location.
+//==============================================================================
+unsigned int NetworkAddress::readRaw(const unsigned char* buffer)
+{
+    memcpy(network_address_raw, buffer, length_bytes);
+    return length_bytes;
+}
+
+//==============================================================================
+// Writes the field to the "buffer" memory location.
+//==============================================================================
+unsigned int NetworkAddress::writeRaw(unsigned char* buffer) const
+{
+    memcpy(buffer, network_address_raw, length_bytes);
+    return length_bytes;
+}
+
+//==============================================================================
+// Equality comparison, NetworkAddress == NetworkAddress
+//==============================================================================
+bool operator==(const NetworkAddress& network_address1,
+                const NetworkAddress& network_address2)
+{
+    if (network_address1.getLengthBytes() != network_address2.getLengthBytes())
+    {
+        return false;
+    }
+
+    // We know both addresses have equal length at this point
+    unsigned int length_bytes = network_address1.getLengthBytes();
+
+    unsigned char network_address_raw1[length_bytes];
+    unsigned char network_address_raw2[length_bytes];
+
+    network_address1.writeRaw(network_address_raw1);
+    network_address2.writeRaw(network_address_raw2);
+
+    return !memcmp(&network_address_raw1,
+                   &network_address_raw2,
+                   length_bytes);
+}
+
+//==============================================================================
+// Inequality comparison, NetworkAddress != NetworkAddress
+//==============================================================================
+bool operator!=(const NetworkAddress& network_address1,
+                const NetworkAddress& network_address2)
+{
+    return !(network_address1 == network_address2);
+}
---
+++ networking/NetworkAddress.hpp
@@ -0,0 +1,77 @@
+#if !defined NETWORK_ADDRESS_HPP
+#define NETWORK_ADDRESS_HPP
+
+#include "Field.hpp"
+
+class NetworkAddress : public Field
+{
+public:
+
+    // Will dynamically allocate an address that is "length_bytes" in size
+    NetworkAddress(unsigned int length_bytes);
+
+    // Will use the memory at "buffer" (of size "length_bytes") as the raw
+    // network address; will not dynamically allocate memory
+    NetworkAddress(unsigned char* buffer,
+                   unsigned int   length_bytes);
+
+    virtual ~NetworkAddress();
+
+    // Reads the field from the "buffer" memory location.
+    virtual unsigned int readRaw(const unsigned char* buffer);
+
+    // Writes the field to the "buffer" memory location.
+    virtual unsigned int writeRaw(unsigned char* buffer) const;
+
+    // Octet access and mutation
+    unsigned char getOctet(unsigned int octet) const;
+    void setOctet(unsigned int octet, unsigned char value);
+
+    // Returns the size of this field in bytes.  This will equal the number of
+    // bytes written by writeRaw() and read by readRaw().
+    virtual unsigned int getLengthBytes() const;
+
+private:
+
+    // Raw network address is stored at this location
+    unsigned char* network_address_raw;
+
+    // Raw network address is this many bytes in length
+    unsigned int length_bytes;
+
+    // Does this class own the memory at "network_address_raw"?
+    bool network_address_raw_owned;
+};
+
+inline unsigned char NetworkAddress::getOctet(unsigned int octet) const
+{
+    if (octet >= length_bytes)
+    {
+        throw std::out_of_range("Out-of-range octet specified");
+    }
+
+    return network_address_raw[octet];
+}
+
+inline void NetworkAddress::setOctet(unsigned int octet, unsigned char value)
+{
+    if (octet >= length_bytes)
+    {
+        throw std::out_of_range("Out-of-range octet specified");
+    }
+
+    network_address_raw[octet] = value;
+}
+
+inline unsigned int NetworkAddress::getLengthBytes() const
+{
+    return length_bytes;
+}
+
+bool operator==(const NetworkAddress& network_address1,
+                const NetworkAddress& network_address2);
+
+bool operator!=(const NetworkAddress& network_address1,
+                const NetworkAddress& network_address2);
+
+#endif
