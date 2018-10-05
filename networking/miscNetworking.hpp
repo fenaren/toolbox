7d91e11582ca83d32b1f657bbf71a09d0bf3bf63
Leigh Garbs <leighgarbs@gmail.com>
2018-10-04 21:32:48 -0400

    Byteswap implemented, test needs filling out

--- networking/miscNetworking.hpp
+++ networking/miscNetworking.hpp
@@ -15,6 +15,10 @@ namespace miscNetworking
     // Retrieves an IP address corresponding to the given interface name
     bool getIpv4Address(const std::string& interface_name,
                         Ipv4Address&       ipv4_address);
+
+    // Byteswaps the buffer of memory at "buffer" of length "len".  For example
+    // a "len" value of 4 would be used for swapping a single 32-bit integer
+    void byteswap(unsigned char* buffer, unsigned int len);
 }
 
 #endif
