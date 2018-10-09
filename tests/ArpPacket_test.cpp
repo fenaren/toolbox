cd46eecf5d82a4f133daa8aead9d2bc25cc0b717
Leigh Garbs <leighgarbs@gmail.com>
2018-10-08 20:42:12 -0400

    Added another minor bit to the test

--- tests/ArpPacket_test.cpp
+++ tests/ArpPacket_test.cpp
@@ -21,8 +21,13 @@ ArpPacket_test::~ArpPacket_test()
 //==============================================================================
 Test::Result ArpPacket_test::run()
 {
-    ArpPacket arp_packet(6, 4);
-    MUST_BE_TRUE(arp_packet.getLengthBytes() == 28);
+    // Ethernet and IPv4
+    ArpPacket arp_packet1(6, 4);
+    MUST_BE_TRUE(arp_packet1.getLengthBytes() == 28);
+
+    // Ethernet and IPv6
+    ArpPacket arp_packet2(6, 16);
+    MUST_BE_TRUE(arp_packet2.getLengthBytes() == 52);
 
     return Test::PASSED;
 }
