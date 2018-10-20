415c0e30520ae0e0d3bdf9e99dc6fd732ddcd062
Leigh Garbs <leighgarbs@gmail.com>
2018-10-20 15:20:12 -0400

    Explanatory documentation added

--- MemoryMode.hpp
+++ MemoryMode.hpp
@@ -1,6 +1,15 @@
 #if !defined MEMORY_MODE_HPP
 #define MEMORY_MODE_HPP
 
+// This is used by constructs which can store relevant data internally or
+// externally, depending on how they are configured or used.  For example, a
+// class may be programmed to perform a relatively expensive memory allocation
+// internally, or optionally not perform that allocation and use pre-allocated
+// memory that has been prepared by some other means.
+
+// This concept seems like it could be implemented as a simple abstract class
+// (an "interface"), but doesn't seem worth the trouble.
+
 enum MemoryMode
 {
     MEMORY_INTERNAL,
