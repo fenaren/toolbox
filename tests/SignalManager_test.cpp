b0ce82153b20b6f05228c32264ddb45f8893b8b3
Leigh Garbs <leighgarbs@gmail.com>
2018-10-27 17:32:05 -0400

    Test needs to actually signal and handle all the signals

--- tests/SignalManager_test.cpp
+++ tests/SignalManager_test.cpp
@@ -23,10 +23,18 @@ SignalManager_test::~SignalManager_test()
 //==============================================================================
 Test::Result SignalManager_test::run()
 {
-    SignalManager signal_manager;
+    SignalManager* signal_manager = 0;
+    try
+    {
+        signal_manager = new SignalManager();
+    }
+    catch (std::runtime_error& ex)
+    {
+        return Test::SKIPPED;
+    }
 
     std::unordered_set<int> supported_signals;
-    signal_manager.getSupportedSignals(supported_signals);
+    signal_manager->getSupportedSignals(supported_signals);
     std::cout << supported_signals.size() << " supported signal(s)\n";
 
     for (std::unordered_set<int>::const_iterator i = supported_signals.begin();
@@ -34,7 +42,7 @@ Test::Result SignalManager_test::run()
          ++i)
     {
         std::string signal_name;
-        signal_manager.getSignalName(*i, signal_name);
+        signal_manager->getSignalName(*i, signal_name);
         std::cout << std::setw(2) << *i << " " << signal_name << "\n";
     }
 
