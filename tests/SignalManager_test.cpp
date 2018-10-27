f52a5ad5cdd73f780606e1a567e26e74b6cba55a
Leigh Garbs <leighgarbs@gmail.com>
2018-10-27 16:09:15 -0400

    WIP

---
+++ tests/SignalManager_test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "SignalManager_test.hpp"
+
+#include "SignalManager.hpp"
+#include "Test.hpp"
+#include "TestMacros.hpp"
+
+TEST_PROGRAM_MAIN(SignalManager_test);
+
+//==============================================================================
+SignalManager_test::SignalManager_test()
+{
+}
+
+//==============================================================================
+SignalManager_test::~SignalManager_test()
+{
+}
+
+//==============================================================================
+Test::Result SignalManager_test::run()
+{
+    return Test::PASSED;
+}
