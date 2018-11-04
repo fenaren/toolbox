961c2591ae9261885be46c72cb82d4bc9cca1aba
Leigh Garbs <leighgarbs@gmail.com>
2018-11-04 16:14:33 -0500

    Cleaned up ArpPacket_test a bit

--- tests/ArpPacket_test.hpp
+++ tests/ArpPacket_test.hpp
@@ -3,6 +3,8 @@
 
 #include "Test.hpp"
 
+#include "NetworkAddress.hpp"
+
 class ArpPacket_test : public Test
 {
 public:
@@ -13,6 +15,25 @@ public:
 
     // Implements the test
     virtual Test::Result run();
+
+private:
+
+    bool isLengthBad(const NetworkAddress& na1,
+                     const NetworkAddress& na2,
+                     const NetworkAddress& na3,
+                     const NetworkAddress& na4);
+
+    static const unsigned int HLEN  = 6;
+    static const unsigned int PLEN4 = 4;
+    static const unsigned int PLEN6 = 16;
+    static const unsigned int HTYPE = 1;
+    static const unsigned int PTYPE = 2048;
+    static const unsigned int OPER  = 42;
+
+    NetworkAddress namac_good;
+    NetworkAddress namac_bad;
+    NetworkAddress na4_good;
+    NetworkAddress na4_bad;
 };
 
 #endif
