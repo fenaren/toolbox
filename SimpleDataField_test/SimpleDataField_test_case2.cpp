54cd41713f2498421430f1d54be8fee470ae16c4
Leigh Garbs <leighgarbs@gmail.com>
2019-01-25 16:32:12 -0500

    Second SimpleDataField test case added, not filled in yet

---
+++ SimpleDataField_test/SimpleDataField_test_case2.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <iostream>
+
+#include "SimpleDataField.hpp"
+#include "Test.hpp"
+#include "TestMacros.hpp"
+
+TEST_HEADER(SimpleDataField_test_case2);
+TEST_CONSTRUCTOR_DESTRUCTOR(SimpleDataField_test_case2);
+
+//==============================================================================
+Test::Result SimpleDataField_test_case2::body()
+{
+    return Test::PASSED;
+}
