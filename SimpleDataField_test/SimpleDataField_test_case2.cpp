9062fe5db94927448c4db0919840337c032cfdf1
Leigh Garbs <leighgarbs@gmail.com>
2019-01-26 20:25:39 -0500

    WIP

--- SimpleDataField_test/SimpleDataField_test_case2.cpp
+++ SimpleDataField_test/SimpleDataField_test_case2.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <vector>
 
 #include "SimpleDataField.hpp"
 #include "Test.hpp"
@@ -10,17 +12,45 @@ TEST_HEADER(SimpleDataField_test_case2);
 TEST_CONSTRUCTOR_DESTRUCTOR(SimpleDataField_test_case2);
 
 //==============================================================================
+template <class T>
+bool writeRawTest(unsigned int                     offset_bits,
+                  const std::vector<std::uint8_t>& shouldbe)
+{
+    SimpleDataField<T> test_sdf_uchar(0);
+
+    std::uint8_t test_sdf_uchar_buf[sizeof(T) + 1];
+    memset(test_sdf_uchar_buf, 0xff, sizeof(T) + 1);
+    test_sdf_uchar.writeRaw(
+        test_sdf_uchar_buf, misc::getByteOrder(), offset_bits);
+
+    bool all_match = true;
+    for (unsigned int i = 0; i < sizeof(T) + 1; ++i)
+    {
+        std::cout << static_cast<unsigned int>(test_sdf_uchar_buf[i]) << " ";
+        if (test_sdf_uchar_buf[i] != shouldbe[i])
+        {
+            all_match = false;
+            std::cout << "(WRONG should be "
+                      << static_cast<unsigned int>(shouldbe[i]) << ") ";
+        }
+    }
+    std::cout << "\n";
+
+    return all_match;
+}
+
+//==============================================================================
 Test::Result SimpleDataField_test_case2::body()
 {
-    SimpleDataField<unsigned char> test_sdf_uchar(255);
-
-    std::uint8_t destbuf[2];
-    memset(destbuf, 0, 2);
-
-    test_sdf_uchar.writeRaw(destbuf, misc::getByteOrder(), 8);
-
-    std::cout << static_cast<unsigned int>(destbuf[0]) << " "
-              << static_cast<unsigned int>(destbuf[1]) << "\n";
+    for (unsigned int i = 0; i <= BITS_PER_BYTE; ++i)
+    {
+        MUST_BE_TRUE(
+            writeRawTest<unsigned char>(
+                i,
+                std::vector<std::uint8_t>(
+                    {static_cast<std::uint8_t>(std::pow(2, i) - 1),
+                     static_cast<std::uint8_t>(255 - (std::pow(2, i) - 1))})));
+    }
 
     return Test::PASSED;
 }
