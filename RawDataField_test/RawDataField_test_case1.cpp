231d9cbb9624e62ebfa579135a03d0e90cadb95d
Leigh Garbs <leighgarbs@gmail.com>
2019-03-03 12:14:08 -0500

    Reinstated a mistakenly-removed part, cleaned up a bit

--- RawDataField_test/RawDataField_test_case1.cpp
+++ RawDataField_test/RawDataField_test_case1.cpp
@@ -13,10 +13,10 @@ Test::Result RawDataField_test_case1::body()
 {
     // Makes no sense to create an bit field of 0 bytes but we should test
     // it here
-    unsigned int test_bf2_len = 10;
-    RawDataField test_bf1(0, misc::BYTES); // Dynamic allocation
-    RawDataField test_bf2(test_bf2_len, misc::BYTES);
-    RawDataField test_bf3(test_bf2);     // Tests the copy constructor
+    unsigned int test_rdf2_len = 10;
+    RawDataField test_rdf1(0, misc::BYTES); // Dynamic allocation
+    RawDataField test_rdf2(test_rdf2_len, misc::BYTES);
+    RawDataField test_rdf3(test_rdf2);     // Tests the copy constructor
 
     unsigned int test_data_len = 20;
     unsigned char test_data[test_data_len];
@@ -26,27 +26,36 @@ Test::Result RawDataField_test_case1::body()
     }
 
     // Tests getLengthBytes
-    RawDataField test_bf4(
+    RawDataField test_rdf4(
         test_data, test_data_len, misc::BYTES, false);
-    MUST_BE_TRUE(test_bf4.getLengthBytes() == test_data_len);
+    MUST_BE_TRUE(test_rdf4.getLengthBytes() == test_data_len);
 
     // Tests writeRaw
     unsigned char test_data2[test_data_len];
-    test_bf4.DataField::writeRaw(test_data2);
+    test_rdf4.DataField::writeRaw(test_data2);
     MUST_BE_TRUE(memcmp(test_data, test_data2, test_data_len) == 0);
 
     // Tests readRaw, equality, inequality
-    RawDataField test_bf5(test_data_len, misc::BYTES);
-    test_bf5.DataField::readRaw(test_data2);
-    MUST_BE_TRUE(test_bf4 == test_bf5);
-    MUST_BE_FALSE(test_bf4 != test_bf5);
+    RawDataField test_rdf5(test_data_len, misc::BYTES);
+    test_rdf5.DataField::readRaw(test_data2);
+    MUST_BE_TRUE(test_rdf4 == test_rdf5);
+    MUST_BE_FALSE(test_rdf4 != test_rdf5);
+
+    // Tests getByte, setByte
+    for (unsigned int i = 0; i < test_data_len; i++)
+    {
+        MUST_BE_TRUE(test_data[i] == test_rdf4.getByte(i));
+
+        test_rdf4.setByte(i, 0);
+        MUST_BE_TRUE(test_data[i] == 0);
+    }
 
     // Tests assignment
     memset(test_data,  0, test_data_len);
     memset(test_data2, 1, test_data_len);
-    RawDataField test_bf6(
+    RawDataField test_rdf6(
         test_data2, test_data_len, misc::BYTES, false);
-    test_bf4 = test_bf6;
+    test_rdf4 = test_rdf6;
     MUST_BE_TRUE(memcmp(test_data, test_data2, test_data_len) == 0);
 
     return Test::PASSED;
