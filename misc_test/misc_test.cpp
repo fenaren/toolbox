b0de935cd6d1b0fd066b6c5eea507a7714b18d4f
Leigh Garbs <leighgarbs@gmail.com>
2019-04-06 16:59:14 -0400

    Finished misc_test

--- misc_test/misc_test.cpp
+++ misc_test/misc_test.cpp
@@ -8,7 +8,10 @@
 
 TEST_CASES_PROGRAM_BEGIN(misc_test)
 TEST(Byteswap_InPlace)
+TEST(Byteswap_InPlaceTemplate)
 TEST(Byteswap_OutOfPlace)
+TEST(Endian_OperatorNot)
+TEST(SmallestMultipleOfXGreaterOrEqualToY)
 TEST_CASES_PROGRAM_END(misc_test)
 
 const unsigned int SIZE = 8;
@@ -20,7 +23,9 @@ void initializeSquareMatrixSwapped(std::uint8_t (&m)[SIZE][SIZE]);
 void misc_test::addTestCases()
 {
     addTestCase(new Byteswap_InPlace());
-//    addTestCase(new Byteswap_OutOfPlace());
+    addTestCase(new Byteswap_InPlaceTemplate());
+    addTestCase(new Byteswap_OutOfPlace());
+    addTestCase(new Endian_OperatorNot());
 }
 
 //==============================================================================
@@ -40,59 +45,52 @@ Test::Result misc_test::Byteswap_InPlace::body()
 
     return Test::PASSED;
 }
-/*
+
 //==============================================================================
-Test::Result misc_test::Byteswap_OutOfPlace::body()
+Test::Result misc_test::Byteswap_InPlaceTemplate::body()
 {
-    unsigned char a1_backwards[] = {0};
-    unsigned char a2_backwards[] = {0, 0};
-    unsigned char a3_backwards[] = {0, 0, 0};
-    unsigned char a4_backwards[] = {0, 0, 0, 0};
-    unsigned char a5_backwards[] = {0, 0, 0, 0, 0};
-    unsigned char a6_backwards[] = {0, 0, 0, 0, 0, 0};
-    unsigned char a7_backwards[] = {0, 0, 0, 0, 0, 0, 0};
-    unsigned char a8_backwards[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    std::uint32_t someuint = 3091572734;
+    misc::byteswap(someuint);
 
-    misc::byteswap(a1_backwards, a1, 1);
-    misc::byteswap(a2_backwards, a2, 2);
-    misc::byteswap(a3_backwards, a3, 3);
-    misc::byteswap(a4_backwards, a4, 4);
-    misc::byteswap(a5_backwards, a5, 5);
-    misc::byteswap(a6_backwards, a6, 6);
-    misc::byteswap(a7_backwards, a7, 7);
-    misc::byteswap(a8_backwards, a8, 8);
-
-    MUST_BE_TRUE(memcmp(a1_before, a1_after, 1) == 0);
-    MUST_BE_TRUE(memcmp(a2_before, a2_after, 2) == 0);
-    MUST_BE_TRUE(memcmp(a3_before, a3_after, 3) == 0);
-    MUST_BE_TRUE(memcmp(a4_before, a4_after, 4) == 0);
-    MUST_BE_TRUE(memcmp(a5_before, a5_after, 5) == 0);
-    MUST_BE_TRUE(memcmp(a6_before, a6_after, 6) == 0);
-    MUST_BE_TRUE(memcmp(a7_before, a7_after, 7) == 0);
-    MUST_BE_TRUE(memcmp(a8_before, a8_after, 8) == 0);
+    // Used MacOS built-in calculator app to calcluate this
+    MUST_BE_TRUE(someuint == 4272375224);
 
     return Test::PASSED;
 }
-/*
+
+//==============================================================================
+Test::Result misc_test::Byteswap_OutOfPlace::body()
+{
+    // Use two test matrices, one with the rows initialized in opposite order
+    std::uint8_t workspace1[SIZE][SIZE];
+    std::uint8_t workspace2[SIZE][SIZE];
+    std::uint8_t workspace3[SIZE][SIZE];
+    initializeSquareMatrix(workspace1);
+    initializeSquareMatrix(workspace2);
+    initializeSquareMatrixSwapped(workspace3);
+
+    for (unsigned int i = 0; i < SIZE; ++i)
+    {
+        misc::byteswap(workspace2[i], workspace1[i], i + 1);
+        MUST_BE_TRUE(memcmp(workspace2[i], workspace3[i], i + 1) == 0);
+    }
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result misc_test::Endian_OperatorNot::body()
+{
     // Test operator! on values of the ByteOrder enumeration
     MUST_BE_TRUE(misc::ENDIAN_LITTLE == !misc::ENDIAN_BIG);
     MUST_BE_TRUE(misc::ENDIAN_BIG    == !misc::ENDIAN_LITTLE);
 
-    // At this point we're satisfied with he performance of the other two
-    // byteswap functions, so we should be okay to use those to test the
-    // template byteswap function
-    std::uint32_t a4_int;
-    memcpy(&a4_int, a4_before, sizeof(std::uint32_t));
-    misc::byteswap(a4_int);
-    misc::byteswap(a4_after, 4);
+    return Test::PASSED;
+}
 
-    // a4_before and a4_after were the same before this test, and we should have
-    // applied the same operation to both of them, so they should be the same
-    // afterwards
-    MUST_BE_TRUE(
-        memcmp(a4_after, reinterpret_cast<unsigned char*>(&a4_int), 4) == 0);
-
-    // A bunch of smallestMultiple tests
+//==============================================================================
+Test::Result misc_test::SmallestMultipleOfXGreaterOrEqualToY::body()
+{
     MUST_BE_TRUE(misc::smallestMultipleOfXGreaterOrEqualToY(1,  2)  == 2);
     MUST_BE_TRUE(misc::smallestMultipleOfXGreaterOrEqualToY(2,  2)  == 2);
     MUST_BE_TRUE(misc::smallestMultipleOfXGreaterOrEqualToY(2,  5)  == 6);
@@ -103,7 +101,6 @@ Test::Result misc_test::Byteswap_OutOfPlace::body()
 
     return Test::PASSED;
 }
-*/
 
 //==============================================================================
 void initializeSquareMatrix(std::uint8_t (&m)[SIZE][SIZE])
