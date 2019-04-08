264cc08fe401db0c5dad9286d1af21a050162e65
Leigh Garbs <leighgarbs@gmail.com>
2019-04-08 18:12:42 -0400

    This works now

--- TemplateClass_test/TemplateClass_test.cpp
+++ TemplateClass_test/TemplateClass_test.cpp
@@ -1,32 +1,19 @@
 #include "Test.hpp"
+#include "TestCases.hpp"
 #include "TestMacros.hpp"
 
-// These two use the macros necessary for defining trivial tests and trivial
-// test cases, respectively.  If using one of these don't use any other macro
-// from TestMacros.hpp.
-
-TRIVIAL_TEST(TemplateClass_test);
-//TRIVIAL_TEST_CASES(TemplateClass_test);
-
-// Use TEST_CASES_HEADER if this test is a TestCases, otherwise use
-// TEST_HEADER.  Only use if not using one of the TRIVIAL_* macros.
-
-//TEST_HEADER(TemplateClass_test);
-//TEST_CASES_HEADER(TemplateClass_test);
-
-// Defines a main() function for this test.  Don't use if this test is used as
-// a test case in a separately-defined TestCases-derived class (that test should
-// define main()).  Only use if not using one of the TRIVIAL_ macros.
-
-//TEST_PROGRAM_MAIN(TemplateClass_test);
-
-// Defines an empty constructor and destructor.  Only use if not using one of
-// the TRIVIAL_* macros.
-
-//TEST_CONSTRUCTOR_DESTRUCTOR(TemplateClass_test);
+TEST_CASES_PROGRAM_BEGIN(TemplateClass_test)
+TEST(Stub)
+TEST_CASES_PROGRAM_END(TemplateClass_test)
 
 //==============================================================================
-Test::Result TemplateClass_test::body()
+void TemplateClass_test::addTestCases()
+{
+    addTestCase(new Stub());
+}
+
+//==============================================================================
+Test::Result TemplateClass_test::Stub::body()
 {
     return Test::PASSED;
 }
