a43dc9ef7612349a802d9fb5e84937ceeebdef46
Leigh Garbs <leighgarbs@gmail.com>
2019-04-26 17:00:51 -0700

    Should work now

--- SignalManager_test/SignalManager_test.cpp
+++ SignalManager_test/SignalManager_test.cpp
@@ -6,30 +6,55 @@
 #include <unordered_set>
 
 #include "SignalManager.hpp"
+
 #include "Test.hpp"
+#include "TestCases.hpp"
 #include "TestMacros.hpp"
 
-TRIVIAL_TEST(SignalManager_test);
+TEST_CASES_PROGRAM_BEGIN(SignalManager_test)
+TEST(PrintSupportedSignalCount)
+TEST(SignalUnknownSignal)
+TEST(CheckUnknownSignalDelivered)
+TEST(GenerateAllSignals)
+TEST_CASES_PROGRAM_END(SignalManager_test)
+
+#define GET_SIGNAL_MANAGER(signal_manager)      \
+    try                                         \
+    {                                           \
+        signal_manager = new SignalManager();   \
+    }                                           \
+    catch (std::runtime_error& ex)              \
+    {                                           \
+        return Test::SKIPPED;                   \
+    }
 
 //==============================================================================
-Test::Result SignalManager_test::body()
+void SignalManager_test::addTestCases()
+{
+    addTestCase(new SignalUnknownSignal());
+    addTestCase(new CheckUnknownSignalDelivered());
+    addTestCase(new GenerateAllSignals());
+}
+
+//==============================================================================
+Test::Result SignalManager_test::SignalUnknownSignal::body()
 {
     SignalManager* signal_manager = 0;
-    try
-    {
-        signal_manager = new SignalManager();
-    }
-    catch (std::runtime_error& ex)
-    {
-        return Test::SKIPPED;
-    }
-
-    std::unordered_set<int> supported_signals;
-    signal_manager->getSupportedSignals(supported_signals);
-    std::cout << supported_signals.size() << " supported signal(s)\n";
+    GET_SIGNAL_MANAGER(signal_manager);
 
     // Give the signal manager a garbage signal and see what happens
     signal_manager->signal(999);
+
+    delete signal_manager;
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result SignalManager_test::CheckUnknownSignalDelivered::body()
+{
+    SignalManager* signal_manager = 0;
+    GET_SIGNAL_MANAGER(signal_manager);
 
     // Give the signal manager another garbage signal and see what happens
     bool signal_caught = false;
@@ -41,7 +66,23 @@ Test::Result SignalManager_test::body()
     {
         signal_caught = true;
     }
+
     MUST_BE_TRUE(signal_caught);
+
+    delete signal_manager;
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result SignalManager_test::GenerateAllSignals::body()
+{
+    SignalManager* signal_manager = 0;
+    GET_SIGNAL_MANAGER(signal_manager);
+
+    std::unordered_set<int> supported_signals;
+    signal_manager->getSupportedSignals(supported_signals);
+    std::cout << supported_signals.size() << " supported signal(s)\n";
 
     // Test that we can signal and handle all the signals
     for (std::unordered_set<int>::const_iterator i = supported_signals.begin();
