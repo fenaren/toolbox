ed9928d10d51b1ee0fb8446edcfd377e5fbf53ce
Leigh Garbs <leighgarbs@gmail.com>
2019-06-16 14:24:29 -0700

    WIP

--- misc.cpp
+++ misc.cpp
@@ -6,7 +6,6 @@
 //==============================================================================
 misc::ByteOrder misc::getByteOrder()
 {
-    int something[4]; // whatever
     unsigned short test_var = 0xff00;
     if (*(unsigned char*)&test_var > 0)
     {
