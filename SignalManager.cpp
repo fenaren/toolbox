5cb2dd2a69b3a28b6a555be76555b87ea914825d
Leigh Garbs <leighgarbs@gmail.com>
2019-07-01 20:44:32 -0700

    WIP

--- SignalManager.cpp
+++ SignalManager.cpp
@@ -31,6 +31,8 @@ extern "C" void handle_signal(int sig)
 SignalManager::SignalManager() :
     signal_manager_impl(0)
 {
+    new int;
+
     // Set the pointer used to forward signals to this SignalManager by
     // handle_signal
     signalmanager_p = this;
