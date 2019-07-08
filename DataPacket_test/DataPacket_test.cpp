c1bb33ba3a33840a9b3fc37ae97a96d2d3fbd1e6
Leigh Garbs <leighgarbs@gmail.com>
2019-07-07 18:05:55 -0700

    WIP

--- DataPacket_test/DataPacket_test.cpp
+++ DataPacket_test/DataPacket_test.cpp
@@ -10,7 +10,6 @@
 #include "Test.hpp"
 #include "TestCases.hpp"
 #include "TestMacros.hpp"
-#include "pullUpdatePush.hpp"
 
 TEST_PROGRAM_MAIN(DataPacket_test)
 
@@ -34,31 +33,172 @@ void DataPacket_test::GetLengthBits::addTestCases()
 }
 
 //==============================================================================
+Test::Result DataPacket_test::ReadRaw::body()
+{
+    DataPacket_test1 dp1;
+    DataPacket_test2* dp2 = dp1.getNestedPacket();
+
+    dp1.setAlignment(3);
+    dp2->setAlignment(3);
+
+    // Set dp to some arbitrarily-chosen values.  We know what these values are
+    // and can test for them later.
+    dp1.setSdfInt(0);
+    dp1.setSdfDouble(0.0);
+    dp2->setSdfFloat1(0.0f);
+    dp2->setSdfFloat2(0.0f);
+    dp2->setSdfChar(0);
+
+    // We're going to manually set up this memory with the known values defined
+    // below, have the packet read it in, and then check that they match
+    std::uint8_t* raw_dp1 = new std::uint8_t[dp1.getLengthBytes()];
+
+    int    dp1_int    = 3;
+    double dp1_double = 4.0;
+    float  dp2_float1 = 1.0f;
+    float  dp2_float2 = 2.0f;
+    char   dp2_char   = 'A';
+
+    unsigned int offset = 0;
+
+    // Grab sdf_int; it should be at the very beginning of the raw packet
+    offset += pushField(raw_dp1, dp1_int, offset);
+
+    // Should be 2 bytes of padding here
+    offset += 2;
+
+    // Grab sdf_double; it should be right after sdf_int, which means an offset
+    // of sizeof(int)
+    offset += pushField(raw_dp1, dp1_double, offset);
+
+    // Should be 1 byte of padding here
+    offset += 1;
+
+    // Grab sdf_float1 from the nested packet; it should be right after all the
+    // fields in the top-level packet, since we're still aligned on 4 bytes
+    offset += pushField(raw_dp1, dp2_float1, offset);
+
+    // Should be 2 bytes of padding here
+    offset += 2;
+
+    // Grab the character out of the nested packet and push an updated and
+    // byteswapped copy of it into its place.  readRaw() will read this out
+    // later, hopefully correctly.
+    offset += pushField(raw_dp1, dp2_float2, offset);
+
+    // Should be 2 bytes of padding here
+    offset += 2;
+
+    // Grab the character out of the nested packet and push an updated and
+    // byteswapped copy of it into its place.  readRaw() will read this out
+    // later, hopefully correctly.
+    offset += pushField(raw_dp1, dp2_char, offset);
+
+    // Now read the field with the functionality under test
+    dp1.DataField::readRaw(raw_dp1);
+
+    // All the values we copied out of the raw packet have to match the values
+    // we originally set them to
+    MUST_BE_TRUE(dp1_int    == dp1.getSdfInt());
+    MUST_BE_TRUE(dp1_double == dp1.getSdfDouble());
+    MUST_BE_TRUE(dp2_float1 == dp2->getSdfFloat1());
+    MUST_BE_TRUE(dp2_float2 == dp2->getSdfFloat2());
+    MUST_BE_TRUE(dp2_char   == dp2->getSdfChar());
+
+    delete [] raw_dp1;
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result DataPacket_test::ReadRawConstBuffer::body()
+{
+    return Test::SKIPPED;
+}
+
+//==============================================================================
+Test::Result DataPacket_test::WriteRaw::body()
+{
+    DataPacket_test1 dp1;
+    DataPacket_test2* dp2 = dp1.getNestedPacket();
+
+    dp1.setAlignment(3);
+    dp2->setAlignment(3);
+
+    // Set dp to some arbitrarily-chosen values.  We know what these values are
+    // and can test for them later.
+    dp1.setSdfInt(3);
+    dp1.setSdfDouble(4.0);
+    dp2->setSdfFloat1(1.0f);
+    dp2->setSdfFloat2(2.0f);
+    dp2->setSdfChar('A');
+
+    // Write out dp1 with a 3-byte alignment and make sure it looks
+    // alright.  Write is done in host byte order.
+    std::uint8_t* raw_dp1 = new std::uint8_t[dp1.getLengthBytes()];
+    dp1.DataField::writeRaw(raw_dp1);
+
+    unsigned int offset = 0;
+
+    // Grab sdf_int; it should be at the very beginning of the raw packet
+    int dp1_int;
+    offset += pullField(dp1_int, raw_dp1, offset);
+
+    // Should be 2 bytes of padding here
+    offset += 2;
+
+    // Grab sdf_double; it should be right after sdf_int, which means an offset
+    // of sizeof(int)
+    double dp1_double;
+    offset += pullField(dp1_double, raw_dp1, offset);
+
+    // Should be 1 byte of padding here
+    offset += 1;
+
+    // Grab sdf_float1 from the nested packet; it should be right after all the
+    // fields in the top-level packet, since we're still aligned on 4 bytes
+    float dp2_float1;
+    offset += pullField(dp2_float1, raw_dp1, offset);
+
+    // Should be 2 bytes of padding here
+    offset += 2;
+
+    // Grab the character out of the nested packet and push an updated and
+    // byteswapped copy of it into its place.  readRaw() will read this out
+    // later, hopefully correctly.
+    float dp2_float2;
+    offset += pullField(dp2_float2, raw_dp1, offset);
+
+    // Should be 2 bytes of padding here
+    offset += 2;
+
+    // Grab the character out of the nested packet and push an updated and
+    // byteswapped copy of it into its place.  readRaw() will read this out
+    // later, hopefully correctly.
+    char dp2_char;
+    offset += pullField(dp2_char, raw_dp1, offset);
+
+    // All the values we copied out of the raw packet have to match the values
+    // we originally set them to
+    MUST_BE_TRUE(dp1_int    == dp1.getSdfInt());
+    MUST_BE_TRUE(dp1_double == dp1.getSdfDouble());
+    MUST_BE_TRUE(dp2_float1 == dp2->getSdfFloat1());
+    MUST_BE_TRUE(dp2_float2 == dp2->getSdfFloat2());
+    MUST_BE_TRUE(dp2_char   == dp2->getSdfChar());
+
+    delete [] raw_dp1;
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result DataPacket_test::WriteRawConst::body()
+{
+    return Test::SKIPPED;
+}
+
+//==============================================================================
 Test::Result DataPacket_test::GetLengthBits::Align1Byte::body()
-{
-    return Test::SKIPPED;
-}
-
-//==============================================================================
-Test::Result DataPacket_test::GetLengthBits::Align2Byte::body()
-{
-    return Test::SKIPPED;
-}
-
-//==============================================================================
-Test::Result DataPacket_test::GetLengthBits::Align3Byte::body()
-{
-    return Test::SKIPPED;
-}
-
-//==============================================================================
-Test::Result DataPacket_test::GetLengthBits::Align4Byte::body()
-{
-    return Test::SKIPPED;
-}
-
-//==============================================================================
-/*Test::Result DataPacket_test::GetLengthBytes_Align1Byte::body()
 {
     DataPacket_test1 dp1;
     DataPacket_test2* dp2 = dp1.getNestedPacket();
@@ -80,7 +220,7 @@ Test::Result DataPacket_test::GetLengthBits::Align4Byte::body()
 }
 
 //==============================================================================
-Test::Result DataPacket_test::GetLengthBytes_Align2Byte::body()
+Test::Result DataPacket_test::GetLengthBits::Align2Byte::body()
 {
     DataPacket_test1 dp1;
     DataPacket_test2* dp2 = dp1.getNestedPacket();
@@ -103,7 +243,7 @@ Test::Result DataPacket_test::GetLengthBytes_Align2Byte::body()
 }
 
 //==============================================================================
-Test::Result DataPacket_test::GetLengthBytes_Align3Byte::body()
+Test::Result DataPacket_test::GetLengthBits::Align3Byte::body()
 {
     DataPacket_test1 dp1;
     DataPacket_test2* dp2 = dp1.getNestedPacket();
@@ -123,7 +263,7 @@ Test::Result DataPacket_test::GetLengthBytes_Align3Byte::body()
 }
 
 //==============================================================================
-Test::Result DataPacket_test::GetLengthBytes_Align4Byte::body()
+Test::Result DataPacket_test::GetLengthBits::Align4Byte::body()
 {
     DataPacket_test1 dp1;
     DataPacket_test2* dp2 = dp1.getNestedPacket();
@@ -144,192 +284,29 @@ Test::Result DataPacket_test::GetLengthBytes_Align4Byte::body()
 }
 
 //==============================================================================
-Test::Result DataPacket_test::PullUpdatePush::body()
+template <class T>
+unsigned int DataPacket_test::pullField(T&            dptest_var,
+                                        std::uint8_t* raw_dptest,
+                                        unsigned int  offset)
 {
-    DataPacket_test1 dp1;
-    DataPacket_test2* dp2 = dp1.getNestedPacket();
+    // Pull a field from the raw packet and swap to make it legible
+    memcpy(&dptest_var, raw_dptest + offset, sizeof(T));
 
-    dp1.setAlignment(3);
-    dp2->setAlignment(3);
-
-    initializeDp(dp1);
-
-    // Write out dp1 with a 3-byte alignment and make sure it looks
-    // alright.  Write is done in host byte order.
-    std::uint8_t* raw_dp1 = new std::uint8_t[dp1.getLengthBytes()];
-    dp1.DataField::writeRaw(raw_dp1);
-
-    unsigned int offset = 0;
-
-    // Grab sdf_int; it should be at the very beginning of the raw packet
-    int dp1_int;
-    int dp1_int_updated;
-    pullUpdatePush(raw_dp1, dp1_int, dp1_int_updated, offset, false);
-
-    // Should be 2 bytes of padding here
-    offset += 2;
-
-    // Grab sdf_double; it should be right after sdf_int, which means an offset
-    // of sizeof(int)
-    double dp1_double;
-    double dp1_double_updated;
-    pullUpdatePush(raw_dp1,
-                   dp1_double,
-                   dp1_double_updated,
-                   offset,
-                   false);
-
-    // Should be 1 byte of padding here
-    offset += 1;
-
-    // Grab sdf_float1 from the nested packet; it should be right after all the
-    // fields in the top-level packet, since we're still aligned on 4 bytes
-    float dp2_float1;
-    float dp2_float1_updated;
-    pullUpdatePush(raw_dp1, dp2_float1, dp2_float1_updated, offset, false);
-
-    // Should be 2 bytes of padding here
-    offset += 2;
-
-    // Grab the character out of the nested packet and push an updated and
-    // byteswapped copy of it into its place.  readRaw() will read this out
-    // later, hopefully correctly.
-    float dp2_float2;
-    float dp2_float2_updated;
-    pullUpdatePush(raw_dp1, dp2_float2, dp2_float2_updated, offset, false);
-
-    // Should be 2 bytes of padding here
-    offset += 2;
-
-    // Grab the character out of the nested packet and push an updated and
-    // byteswapped copy of it into its place.  readRaw() will read this out
-    // later, hopefully correctly.
-    char dp2_char;
-    char dp2_char_updated;
-    pullUpdatePush(raw_dp1, dp2_char, dp2_char_updated, offset, false);
-
-    // All the values we copied out of the raw packet have to match the values
-    // we originally set them to
-    MUST_BE_TRUE(dp1_int    == dp1.getSdfInt());
-    MUST_BE_TRUE(dp1_double == dp1.getSdfDouble());
-    MUST_BE_TRUE(dp2_float1 == dp2->getSdfFloat1());
-    MUST_BE_TRUE(dp2_float2 == dp2->getSdfFloat2());
-    MUST_BE_TRUE(dp2_char   == dp2->getSdfChar());
-
-    // Read the updated raw data and compare against the updated values
-    dp1.DataField::readRaw(raw_dp1);
-
-    // All the values we wrote into the raw packet have to match the values we
-    // retrieved after reading the raw packet
-    MUST_BE_TRUE(dp1_int_updated    == dp1.getSdfInt());
-    MUST_BE_TRUE(dp1_double_updated == dp1.getSdfDouble());
-    MUST_BE_TRUE(dp2_float1_updated == dp2->getSdfFloat1());
-    MUST_BE_TRUE(dp2_float2_updated == dp2->getSdfFloat2());
-    MUST_BE_TRUE(dp2_char_updated   == dp2->getSdfChar());
-
-    delete [] raw_dp1;
-
-    return Test::PASSED;
+    // Bump this for successive calls
+    return offset + sizeof(T);
 }
 
 //==============================================================================
-Test::Result DataPacket_test::PullUpdatePush_Swap::body()
+template <class T>
+unsigned int DataPacket_test::pushField(std::uint8_t* raw_dptest,
+                                        T&            dptest_var,
+                                        unsigned int  offset)
 {
-    // We want to know what endianness this host does NOT have so we can force
-    // byteswapping from the packet routines
-    misc::ByteOrder byteorder_opposite = !misc::getByteOrder();
+    // Pull a field from the raw packet and swap to make it legible
+    memcpy(raw_dptest + offset,
+           reinterpret_cast<std::uint8_t*>(&dptest_var),
+           sizeof(T));
 
-    DataPacket_test1 dp1;
-    DataPacket_test2* dp2 = dp1.getNestedPacket();
-
-    dp1.setAlignment(3);
-    dp2->setAlignment(3);
-
-    initializeDp(dp1);
-
-    // Write out dp1 with a 3-byte alignment and make sure it looks
-    // alright.  Write is done in whatever byte order this host doesn't have.
-    unsigned char* raw_dp1 = new unsigned char[dp1.getLengthBytes()];
-    dp1.writeRaw(raw_dp1, byteorder_opposite);
-
-    unsigned int offset = 0;
-
-    // Grab sdf_int; it should be at the very beginning of the raw packet
-    int dp1_int;
-    int dp1_int_updated;
-    pullUpdatePush(raw_dp1, dp1_int, dp1_int_updated, offset, true);
-
-    // Should be 2 bytes of padding here
-    offset += 2;
-
-    // Grab sdf_double; it should be right after sdf_int, which means an offset
-    // of sizeof(int)
-    double dp1_double;
-    double dp1_double_updated;
-    pullUpdatePush(raw_dp1, dp1_double, dp1_double_updated, offset, true);
-
-    // Should be 1 byte of padding here
-    offset += 1;
-
-    // Grab sdf_float1 from the nested packet; it should be right after all the
-    // fields in the top-level packet, since we're still aligned on 4 bytes
-    float dp2_float1;
-    float dp2_float1_updated;
-    pullUpdatePush(raw_dp1, dp2_float1, dp2_float1_updated, offset, true);
-
-    // Should be 2 bytes of padding here
-    offset += 2;
-
-    // Grab the character out of the nested packet and push an updated and
-    // byteswapped copy of it into its place.  readRaw() will read this out
-    // later, hopefully correctly.
-    float dp2_float2;
-    float dp2_float2_updated;
-    pullUpdatePush(raw_dp1, dp2_float2, dp2_float2_updated, offset, true);
-
-    // Should be 2 bytes of padding here
-    offset += 2;
-
-    // Grab the character out of the nested packet and push an updated and
-    // byteswapped copy of it into its place.  readRaw() will read this out
-    // later, hopefully correctly.
-    char dp2_char;
-    char dp2_char_updated;
-    pullUpdatePush(raw_dp1, dp2_char, dp2_char_updated, offset, true);
-
-    // All the values we copied out of the raw packet have to match the values
-    // we originally set them to
-    MUST_BE_TRUE(dp1_int    == dp1.getSdfInt());
-    MUST_BE_TRUE(dp1_double == dp1.getSdfDouble());
-    MUST_BE_TRUE(dp2_float1 == dp2->getSdfFloat1());
-    MUST_BE_TRUE(dp2_float2 == dp2->getSdfFloat2());
-    MUST_BE_TRUE(dp2_char   == dp2->getSdfChar());
-
-    // Read the updated raw data and compare against the updated values
-    dp1.readRaw(raw_dp1, byteorder_opposite);
-
-    // All the values we wrote into the raw packet have to match the values we
-    // retrieved after reading the raw packet
-    MUST_BE_TRUE(dp1_int_updated    == dp1.getSdfInt());
-    MUST_BE_TRUE(dp1_double_updated == dp1.getSdfDouble());
-    MUST_BE_TRUE(dp2_float1_updated == dp2->getSdfFloat1());
-    MUST_BE_TRUE(dp2_float2_updated == dp2->getSdfFloat2());
-    MUST_BE_TRUE(dp2_char_updated   == dp2->getSdfChar());
-
-    delete [] raw_dp1;
-
-    return Test::PASSED;
+    // Bump this for successive calls
+    return offset + sizeof(T);
 }
-
-//==============================================================================
-void initializeDp(DataPacket_test1& dp)
-{
-    dp.setSdfInt(3);
-    dp.setSdfDouble(4.0);
-
-    DataPacket_test2* dp2 = dp.getNestedPacket();
-    dp2->setSdfFloat1(1.0f);
-    dp2->setSdfFloat2(2.0f);
-    dp2->setSdfChar('A');
-}
-*/
--- DataPacket_test/DataPacket_test.hpp
+++ DataPacket_test/DataPacket_test.hpp
@@ -1,6 +1,8 @@
 #if !defined DATA_PACKET_TEST
 #define DATA_PACKET_TEST
 
+#include <cstdint>
+
 #include "DataPacket_test1.hpp"
 #include "Test.hpp"
 #include "TestCases.hpp"
@@ -8,17 +10,10 @@
 
 TEST_CASES_BEGIN(DataPacket_test)
 
-    TEST_CASES_BEGIN(ReadRaw)
-    TEST_CASES_END(ReadRaw)
-
-    TEST_CASES_BEGIN(ReadRawConstBuffer)
-    TEST_CASES_END(ReadRawConstBuffer)
-
-    TEST_CASES_BEGIN(WriteRaw)
-    TEST_CASES_END(WriteRaw)
-
-    TEST_CASES_BEGIN(WriteRawConst)
-    TEST_CASES_END(WriteRawConst)
+    TEST(ReadRaw)
+    TEST(ReadRawConstBuffer)
+    TEST(WriteRaw)
+    TEST(WriteRawConst)
 
     TEST_CASES_BEGIN(GetLengthBits)
 
@@ -29,11 +24,14 @@ TEST_CASES_BEGIN(DataPacket_test)
 
     TEST_CASES_END(GetLengthBits)
 
-//    TEST(PullUpdatePush)
-//    TEST(PullUpdatePush_Swap)
+    template <class T> static unsigned int pullField(T&            dptest_var,
+                                                     std::uint8_t* raw_dptest,
+                                                     unsigned int  offset);
+
+    template <class T> static unsigned int pushField(std::uint8_t* raw_dptest,
+                                                     T&            dptest_var,
+                                                     unsigned int  offset);
 
 TEST_CASES_END(DataPacket_test)
-
-//void initializeDp(DataPacket_test1& dp);
 
 #endif
