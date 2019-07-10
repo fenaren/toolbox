b748dcda1d9f28fa343abd2c8b546524c569f25f
Leigh Garbs <leighgarbs@gmail.com>
2019-07-09 21:22:07 -0700

    Split this up a bit

--- Log_test/Log_test.cpp
+++ Log_test/Log_test.cpp
@@ -12,21 +12,82 @@ TEST_PROGRAM_MAIN(Log_test);
 //==============================================================================
 void Log_test::addTestCases()
 {
-    ADD_TEST_CASE(WriteWarningError);
+    ADD_TEST_CASE(GreenwichMeanTime);
+    ADD_TEST_CASE(LocalTime);
 }
 
 //==============================================================================
-Test::Result Log_test::WriteWarningError::body()
+void Log_test::GreenwichMeanTime::addTestCases()
+{
+    ADD_TEST_CASE(Normal);
+    ADD_TEST_CASE(Warning);
+    ADD_TEST_CASE(Error);
+}
+
+//==============================================================================
+void Log_test::LocalTime::addTestCases()
+{
+    ADD_TEST_CASE(Normal);
+    ADD_TEST_CASE(Warning);
+    ADD_TEST_CASE(Error);
+}
+
+//==============================================================================
+Test::Result Log_test::GreenwichMeanTime::Normal::body()
 {
     Log log;
+    log.useGreenwichMeanTime();
     log.write("message1");
-    log.writeWarning("message2");
-    log.writeError("message3");
 
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result Log_test::GreenwichMeanTime::Warning::body()
+{
+    Log log;
+    log.useGreenwichMeanTime();
+    log.writeWarning("message1");
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result Log_test::GreenwichMeanTime::Error::body()
+{
+    Log log;
+    log.useGreenwichMeanTime();
+    log.writeError("message1");
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result Log_test::LocalTime::Normal::body()
+{
+    Log log;
     log.useLocalTime();
     log.write("message1");
-    log.writeWarning("message2");
-    log.writeError("message3");
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result Log_test::LocalTime::Warning::body()
+{
+    Log log;
+    log.useLocalTime();
+    log.writeWarning("message1");
+
+    return Test::PASSED;
+}
+
+//==============================================================================
+Test::Result Log_test::LocalTime::Error::body()
+{
+    Log log;
+    log.useLocalTime();
+    log.writeError("message1");
 
     return Test::PASSED;
 }
--- Log_test/Log_test.hpp
+++ Log_test/Log_test.hpp
@@ -7,7 +7,21 @@
 
 TEST_CASES_BEGIN(Log_test)
 
-    TEST(WriteWarningError)
+    TEST_CASES_BEGIN(GreenwichMeanTime)
+
+        TEST(Normal)
+        TEST(Warning)
+        TEST(Error)
+
+    TEST_CASES_END(GreenwichMeanTime)
+
+    TEST_CASES_BEGIN(LocalTime)
+
+        TEST(Normal)
+        TEST(Warning)
+        TEST(Error)
+
+    TEST_CASES_END(LocalTime)
 
 TEST_CASES_END(Log_test)
 
