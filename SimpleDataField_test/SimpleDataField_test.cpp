1099959d3864af0a161e8abca3d39c9da3f476c9
Leigh Garbs <leighgarbs@gmail.com>
2019-07-15 21:48:52 -0700

    WIP

--- SimpleDataField_test/SimpleDataField_test.cpp
+++ SimpleDataField_test/SimpleDataField_test.cpp
@@ -22,268 +22,169 @@ void SimpleDataField_test::addTestCases()
 }
 
 //==============================================================================
-Test::Result SimpleDataField_test::GetLengthBytes::body()
+void SimpleDataField_test::GetLengthBytes::addTestCases()
 {
-    SimpleDataField<char>               test_sdf_char(0);
-    SimpleDataField<double>             test_sdf_double(0);
-    SimpleDataField<float>              test_sdf_float(0);
-    SimpleDataField<int>                test_sdf_int(0);
-    SimpleDataField<long>               test_sdf_long(0);
-    SimpleDataField<long double>        test_sdf_ld(0);
-    SimpleDataField<long long>          test_sdf_ll(0);
-    SimpleDataField<short>              test_sdf_short(0);
-    SimpleDataField<unsigned char>      test_sdf_uc(0);
-    SimpleDataField<unsigned int>       test_sdf_ui(0);
-    SimpleDataField<unsigned long>      test_sdf_ul(0);
-    SimpleDataField<unsigned long long> test_sdf_ull(0);
-    SimpleDataField<unsigned short>     test_sdf_us(0);
+    ADD_TEST_CASE(Char);
+/*    ADD_TEST_CASE(Double);
+    ADD_TEST_CASE(Float);
+    ADD_TEST_CASE(Int);
+    ADD_TEST_CASE(Long);
+    ADD_TEST_CASE(LongDouble);
+    ADD_TEST_CASE(LongLong);
+    ADD_TEST_CASE(Short);
+    ADD_TEST_CASE(UnsignedChar);
+    ADD_TEST_CASE(UnsignedInt);
+    ADD_TEST_CASE(UnsignedLong);
+    ADD_TEST_CASE(UnsignedLongLong);
+    ADD_TEST_CASE(UnsignedShort);*/
+}
 
-    MUST_BE_TRUE(test_sdf_char.getLengthBytes()   == sizeof(char));
-    MUST_BE_TRUE(test_sdf_double.getLengthBytes() == sizeof(double));
-    MUST_BE_TRUE(test_sdf_float.getLengthBytes()  == sizeof(float));
-    MUST_BE_TRUE(test_sdf_int.getLengthBytes()    == sizeof(int));
-    MUST_BE_TRUE(test_sdf_long.getLengthBytes()   == sizeof(long));
-    MUST_BE_TRUE(test_sdf_ld.getLengthBytes()     == sizeof(long double));
-    MUST_BE_TRUE(test_sdf_ll.getLengthBytes()     == sizeof(long long));
-    MUST_BE_TRUE(test_sdf_short.getLengthBytes()  == sizeof(short));
-    MUST_BE_TRUE(test_sdf_uc.getLengthBytes()     == sizeof(unsigned char));
-    MUST_BE_TRUE(test_sdf_ui.getLengthBytes()     == sizeof(unsigned int));
-    MUST_BE_TRUE(test_sdf_ul.getLengthBytes()     == sizeof(unsigned long));
-    MUST_BE_TRUE(test_sdf_ull.getLengthBytes()    == sizeof(unsigned long long));
-    MUST_BE_TRUE(test_sdf_us.getLengthBytes()     == sizeof(unsigned short));
+//==============================================================================
+void SimpleDataField_test::GetValue::addTestCases()
+{
+    ADD_TEST_CASE(Char);
+/*    ADD_TEST_CASE(Double);
+    ADD_TEST_CASE(Float);
+    ADD_TEST_CASE(Int);
+    ADD_TEST_CASE(Long);
+    ADD_TEST_CASE(LongDouble);
+    ADD_TEST_CASE(LongLong);
+    ADD_TEST_CASE(Short);
+    ADD_TEST_CASE(UnsignedChar);
+    ADD_TEST_CASE(UnsignedInt);
+    ADD_TEST_CASE(UnsignedLong);
+    ADD_TEST_CASE(UnsignedLongLong);
+    ADD_TEST_CASE(UnsignedShort);j*/
+}
 
+//==============================================================================
+void SimpleDataField_test::ReadRaw::addTestCases()
+{
+    ADD_TEST_CASE(Char);
+/*    ADD_TEST_CASE(Double);
+    ADD_TEST_CASE(Float);
+    ADD_TEST_CASE(Int);
+    ADD_TEST_CASE(Long);
+    ADD_TEST_CASE(LongDouble);
+    ADD_TEST_CASE(LongLong);
+    ADD_TEST_CASE(Short);
+    ADD_TEST_CASE(UnsignedChar);
+    ADD_TEST_CASE(UnsignedInt);
+    ADD_TEST_CASE(UnsignedLong);
+    ADD_TEST_CASE(UnsignedLongLong);
+    ADD_TEST_CASE(UnsignedShort);*/
+}
+
+//==============================================================================
+void SimpleDataField_test::SimpleAssignment::addTestCases()
+{
+    ADD_TEST_CASE(Char);
+/*    ADD_TEST_CASE(Double);
+    ADD_TEST_CASE(Float);
+    ADD_TEST_CASE(Int);
+    ADD_TEST_CASE(Long);
+    ADD_TEST_CASE(LongDouble);
+    ADD_TEST_CASE(LongLong);
+    ADD_TEST_CASE(Short);
+    ADD_TEST_CASE(UnsignedChar);
+    ADD_TEST_CASE(UnsignedInt);
+    ADD_TEST_CASE(UnsignedLong);
+    ADD_TEST_CASE(UnsignedLongLong);
+    ADD_TEST_CASE(UnsignedShort);*/
+}
+
+//==============================================================================
+void SimpleDataField_test::WriteRaw::addTestCases()
+{
+    ADD_TEST_CASE(Char);
+/*    ADD_TEST_CASE(Double);
+    ADD_TEST_CASE(Float);
+    ADD_TEST_CASE(Int);
+    ADD_TEST_CASE(Long);
+    ADD_TEST_CASE(LongDouble);
+    ADD_TEST_CASE(LongLong);
+    ADD_TEST_CASE(Short);
+    ADD_TEST_CASE(UnsignedChar);
+    ADD_TEST_CASE(UnsignedInt);
+    ADD_TEST_CASE(UnsignedLong);
+    ADD_TEST_CASE(UnsignedLongLong);
+    ADD_TEST_CASE(UnsignedShort);*/
+}
+
+//==============================================================================
+Test::Result SimpleDataField_test::GetLengthBytes::Char::body()
+{
+    return test<char>();
+}
+
+//==============================================================================
+Test::Result SimpleDataField_test::GetValue::Char::body()
+{
+    return test<char>();
+}
+
+//==============================================================================
+Test::Result SimpleDataField_test::ReadRaw::Char::body()
+{
+    return test<char>();
+}
+
+//==============================================================================
+Test::Result SimpleDataField_test::SimpleAssignment::Char::body()
+{
+    return test<char>();
+}
+
+//==============================================================================
+Test::Result SimpleDataField_test::WriteRaw::Char::body()
+{
+    return test<char>();
+}
+
+//==============================================================================
+template <class T> Test::Result SimpleDataField_test::GetLengthBytes::test()
+{
+    SimpleDataField<T> test_sdf;
+    MUST_BE_TRUE(test_sdf.getLengthBytes() == sizeof(T));
     return Test::PASSED;
 }
 
 //==============================================================================
-Test::Result SimpleDataField_test::GetValue::body()
+template <class T> Test::Result SimpleDataField_test::GetValue::test()
 {
-    SimpleDataField<char>               test_sdf_char(0);
-    SimpleDataField<double>             test_sdf_double(0);
-    SimpleDataField<float>              test_sdf_float(0);
-    SimpleDataField<int>                test_sdf_int(0);
-    SimpleDataField<long>               test_sdf_long(0);
-    SimpleDataField<long double>        test_sdf_ld(0);
-    SimpleDataField<long long>          test_sdf_ll(0);
-    SimpleDataField<short>              test_sdf_short(0);
-    SimpleDataField<unsigned char>      test_sdf_uc(0);
-    SimpleDataField<unsigned int>       test_sdf_ui(0);
-    SimpleDataField<unsigned long>      test_sdf_ul(0);
-    SimpleDataField<unsigned long long> test_sdf_ull(0);
-    SimpleDataField<unsigned short>     test_sdf_us(0);
-
-    test_sdf_char   = 1;
-    test_sdf_double = 2;
-    test_sdf_float  = 3;
-    test_sdf_int    = 4;
-    test_sdf_long   = 5;
-    test_sdf_ld     = 6;
-    test_sdf_ll     = 7;
-    test_sdf_short  = 8;
-    test_sdf_uc     = 9;
-    test_sdf_ui     = 10;
-    test_sdf_ul     = 11;
-    test_sdf_ull    = 12;
-    test_sdf_us     = 13;
-
-    MUST_BE_TRUE(test_sdf_char.getValue()   == 1);
-    MUST_BE_TRUE(test_sdf_double.getValue() == 2);
-    MUST_BE_TRUE(test_sdf_float.getValue()  == 3);
-    MUST_BE_TRUE(test_sdf_int.getValue()    == 4);
-    MUST_BE_TRUE(test_sdf_long.getValue()   == 5);
-    MUST_BE_TRUE(test_sdf_ld.getValue()     == 6);
-    MUST_BE_TRUE(test_sdf_ll.getValue()     == 7);
-    MUST_BE_TRUE(test_sdf_short.getValue()  == 8);
-    MUST_BE_TRUE(test_sdf_uc.getValue()     == 9);
-    MUST_BE_TRUE(test_sdf_ui.getValue()     == 10);
-    MUST_BE_TRUE(test_sdf_ul.getValue()     == 11);
-    MUST_BE_TRUE(test_sdf_ull.getValue()    == 12);
-    MUST_BE_TRUE(test_sdf_us.getValue()     == 13);
-
+    SimpleDataField<T> test_sdf(0);
+    test_sdf.simple_data_field = 1;
+    MUST_BE_TRUE(test_sdf.getValue() == 1);
     return Test::PASSED;
 }
 
 //==============================================================================
-Test::Result SimpleDataField_test::ReadRaw::body()
+template <class T> Test::Result SimpleDataField_test::ReadRaw::test()
 {
-    SimpleDataField<char>               test_sdf_char(0);
-    SimpleDataField<double>             test_sdf_double(0);
-    SimpleDataField<float>              test_sdf_float(0);
-    SimpleDataField<int>                test_sdf_int(0);
-    SimpleDataField<long>               test_sdf_long(0);
-    SimpleDataField<long double>        test_sdf_ld(0);
-    SimpleDataField<long long>          test_sdf_ll(0);
-    SimpleDataField<short>              test_sdf_short(0);
-    SimpleDataField<unsigned char>      test_sdf_uc(0);
-    SimpleDataField<unsigned int>       test_sdf_ui(0);
-    SimpleDataField<unsigned long>      test_sdf_ul(0);
-    SimpleDataField<unsigned long long> test_sdf_ull(0);
-    SimpleDataField<unsigned short>     test_sdf_us(0);
-
-    char               test_char   = 1;
-    double             test_double = 2;
-    float              test_float  = 3;
-    int                test_int    = 4;
-    long               test_long   = 5;
-    long double        test_ld     = 6;
-    long long          test_ll     = 7;
-    short              test_short  = 8;
-    unsigned char      test_uc     = 9;
-    unsigned int       test_ui     = 10;
-    unsigned long      test_ul     = 11;
-    unsigned long long test_ull    = 12;
-    unsigned short     test_us     = 13;
-
-    test_sdf_char.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_char));
-    // cppcheck-suppress invalidPointerCast
-    test_sdf_double.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_double));
-    // cppcheck-suppress invalidPointerCast
-    test_sdf_float.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_float));
-    test_sdf_int.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_int));
-    test_sdf_long.DataField::readRaw(
-        reinterpret_cast<std::uint8_t*>(&test_long));
-    // cppcheck-suppress invalidPointerCast
-    test_sdf_ld.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_ld));
-    test_sdf_ll.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_ll));
-    test_sdf_short.DataField::readRaw(
-        reinterpret_cast<std::uint8_t*>(&test_short));
-    test_sdf_uc.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_uc));
-    test_sdf_ui.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_ui));
-    test_sdf_ul.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_ul));
-    test_sdf_ull.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_ull));
-    test_sdf_us.DataField::readRaw(reinterpret_cast<std::uint8_t*>(&test_us));
-
-    MUST_BE_TRUE(test_sdf_char.getValue()   == 1);
-    MUST_BE_TRUE(test_sdf_double.getValue() == 2);
-    MUST_BE_TRUE(test_sdf_float.getValue()  == 3);
-    MUST_BE_TRUE(test_sdf_int.getValue()    == 4);
-    MUST_BE_TRUE(test_sdf_long.getValue()   == 5);
-    MUST_BE_TRUE(test_sdf_ld.getValue()     == 6);
-    MUST_BE_TRUE(test_sdf_ll.getValue()     == 7);
-    MUST_BE_TRUE(test_sdf_short.getValue()  == 8);
-    MUST_BE_TRUE(test_sdf_uc.getValue()     == 9);
-    MUST_BE_TRUE(test_sdf_ui.getValue()     == 10);
-    MUST_BE_TRUE(test_sdf_ul.getValue()     == 11);
-    MUST_BE_TRUE(test_sdf_ull.getValue()    == 12);
-    MUST_BE_TRUE(test_sdf_us.getValue()     == 13);
-
+    SimpleDataField<T> test_sdf(0);
+    T intrinsic_type = 1;
+    test_sdf.DataField::readRaw(
+        reinterpret_cast<std::uint8_t*>(&intrinsic_type));
+    MUST_BE_TRUE(test_sdf.getValue() == 1);
     return Test::PASSED;
 }
 
 //==============================================================================
-Test::Result SimpleDataField_test::SimpleAssignment::body()
+template <class T> Test::Result SimpleDataField_test::SimpleAssignment::test()
 {
-    SimpleDataField<char>               test_sdf_char;
-    SimpleDataField<double>             test_sdf_double;
-    SimpleDataField<float>              test_sdf_float;
-    SimpleDataField<int>                test_sdf_int;
-    SimpleDataField<long>               test_sdf_long;
-    SimpleDataField<long double>        test_sdf_ld;
-    SimpleDataField<long long>          test_sdf_ll;
-    SimpleDataField<short>              test_sdf_short;
-    SimpleDataField<unsigned char>      test_sdf_uc;
-    SimpleDataField<unsigned int>       test_sdf_ui;
-    SimpleDataField<unsigned long>      test_sdf_ul;
-    SimpleDataField<unsigned long long> test_sdf_ull;
-    SimpleDataField<unsigned short>     test_sdf_us;
-
-    test_sdf_char.simple_data_field   = 1;
-    test_sdf_double.simple_data_field = 2;
-    test_sdf_float.simple_data_field  = 3;
-    test_sdf_int.simple_data_field    = 4;
-    test_sdf_long.simple_data_field   = 5;
-    test_sdf_ld.simple_data_field     = 6;
-    test_sdf_ll.simple_data_field     = 7;
-    test_sdf_short.simple_data_field  = 8;
-    test_sdf_uc.simple_data_field     = 9;
-    test_sdf_ui.simple_data_field     = 10;
-    test_sdf_ul.simple_data_field     = 11;
-    test_sdf_ull.simple_data_field    = 12;
-    test_sdf_us.simple_data_field     = 13;
-
-    MUST_BE_TRUE(test_sdf_char.simple_data_field   == 1);
-    MUST_BE_TRUE(test_sdf_double.simple_data_field == 2);
-    MUST_BE_TRUE(test_sdf_float.simple_data_field  == 3);
-    MUST_BE_TRUE(test_sdf_int.simple_data_field    == 4);
-    MUST_BE_TRUE(test_sdf_long.simple_data_field   == 5);
-    MUST_BE_TRUE(test_sdf_ld.simple_data_field     == 6);
-    MUST_BE_TRUE(test_sdf_ll.simple_data_field     == 7);
-    MUST_BE_TRUE(test_sdf_short.simple_data_field  == 8);
-    MUST_BE_TRUE(test_sdf_uc.simple_data_field     == 9);
-    MUST_BE_TRUE(test_sdf_ui.simple_data_field     == 10);
-    MUST_BE_TRUE(test_sdf_ul.simple_data_field     == 11);
-    MUST_BE_TRUE(test_sdf_ull.simple_data_field    == 12);
-    MUST_BE_TRUE(test_sdf_us.simple_data_field     == 13);
-
-    return Test::PASSED;
+    SimpleDataField<T> test_sdf;
+    test_sdf = 1;
+    MUST_BE_TRUE(test_sdf.simple_data_field == 1);
+    return Test::SKIPPED;
 }
 
 //==============================================================================
-Test::Result SimpleDataField_test::WriteRaw::body()
+template <class T> Test::Result SimpleDataField_test::WriteRaw::test()
 {
-    SimpleDataField<char>               test_sdf_char(1);
-    SimpleDataField<double>             test_sdf_double(2);
-    SimpleDataField<float>              test_sdf_float(3);
-    SimpleDataField<int>                test_sdf_int(4);
-    SimpleDataField<long>               test_sdf_long(5);
-    SimpleDataField<long double>        test_sdf_ld(6);
-    SimpleDataField<long long>          test_sdf_ll(7);
-    SimpleDataField<short>              test_sdf_short(8);
-    SimpleDataField<unsigned char>      test_sdf_uc(9);
-    SimpleDataField<unsigned int>       test_sdf_ui(10);
-    SimpleDataField<unsigned long>      test_sdf_ul(11);
-    SimpleDataField<unsigned long long> test_sdf_ull(12);
-    SimpleDataField<unsigned short>     test_sdf_us(13);
-
-    char               test_char   = 0;
-    double             test_double = 0;
-    float              test_float  = 0;
-    int                test_int    = 0;
-    long               test_long   = 0;
-    long double        test_ld     = 0;
-    long long          test_ll     = 0;
-    short              test_short  = 0;
-    unsigned char      test_uc     = 0;
-    unsigned int       test_ui     = 0;
-    unsigned long      test_ul     = 0;
-    unsigned long long test_ull    = 0;
-    unsigned short     test_us     = 0;
-
-    test_sdf_char.DataField::writeRaw(
-        reinterpret_cast<std::uint8_t*>(&test_char));
-    // cppcheck-suppress invalidPointerCast
-    test_sdf_double.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_double));
-    // cppcheck-suppress invalidPointerCast
-    test_sdf_float.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_float));
-    test_sdf_int.DataField::writeRaw(
-        reinterpret_cast<std::uint8_t*>(&test_int));
-    test_sdf_long.DataField::writeRaw(
-        reinterpret_cast<std::uint8_t*>(&test_long));
-    // cppcheck-suppress invalidPointerCast
-    test_sdf_ld.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_ld));
-    test_sdf_ll.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_ll));
-    test_sdf_short.DataField::writeRaw(
-        reinterpret_cast<std::uint8_t*>(&test_short));
-    test_sdf_uc.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_uc));
-    test_sdf_ui.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_ui));
-    test_sdf_ul.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_ul));
-    test_sdf_ull.DataField::writeRaw(
-        reinterpret_cast<std::uint8_t*>(&test_ull));
-    test_sdf_us.DataField::writeRaw(reinterpret_cast<std::uint8_t*>(&test_us));
-
-    MUST_BE_TRUE(test_char   == 1);
-    MUST_BE_TRUE(test_double == 2);
-    MUST_BE_TRUE(test_float  == 3);
-    MUST_BE_TRUE(test_int    == 4);
-    MUST_BE_TRUE(test_long   == 5);
-    MUST_BE_TRUE(test_ld     == 6);
-    MUST_BE_TRUE(test_ll     == 7);
-    MUST_BE_TRUE(test_short  == 8);
-    MUST_BE_TRUE(test_uc     == 9);
-    MUST_BE_TRUE(test_ui     == 10);
-    MUST_BE_TRUE(test_ul     == 11);
-    MUST_BE_TRUE(test_ull    == 12);
-    MUST_BE_TRUE(test_us     == 13);
-
+    SimpleDataField<T> test_sdf(1);
+    T intrinsic_type = 0;
+    test_sdf.DataField::writeRaw(
+        reinterpret_cast<std::uint8_t*>(&intrinsic_type));
+    MUST_BE_TRUE(intrinsic_type == 1);
     return Test::PASSED;
 }
--- SimpleDataField_test/SimpleDataField_test.hpp
+++ SimpleDataField_test/SimpleDataField_test.hpp
@@ -7,11 +7,105 @@
 
 TEST_CASES_BEGIN(SimpleDataField_test);
 
-    TEST(GetLengthBytes)
-    TEST(GetValue)
-    TEST(ReadRaw)
-    TEST(SimpleAssignment)
-    TEST(WriteRaw)
+    TEST_CASES_BEGIN(GetLengthBytes)
+
+        TEST(Char)
+        TEST(Double)
+        TEST(Float)
+        TEST(Int)
+        TEST(Long)
+        TEST(LongDouble)
+        TEST(LongLong)
+        TEST(Short)
+        TEST(UnsignedChar)
+        TEST(UnsignedInt)
+        TEST(UnsignedLong)
+        TEST(UnsignedLongLong)
+        TEST(CaseUnsignedShort)
+
+        template <class T> static Test::Result test();
+
+    TEST_CASES_END(GetLengthBytes)
+
+    TEST_CASES_BEGIN(GetValue)
+
+        TEST(Char)
+        TEST(Double)
+        TEST(Float)
+        TEST(Int)
+        TEST(Long)
+        TEST(LongDouble)
+        TEST(LongLong)
+        TEST(Short)
+        TEST(UnsignedChar)
+        TEST(UnsignedInt)
+        TEST(UnsignedLong)
+        TEST(UnsignedLongLong)
+        TEST(UnsignedShort)
+
+        template <class T> static Test::Result test();
+
+    TEST_CASES_END(GetValue)
+
+    TEST_CASES_BEGIN(ReadRaw)
+
+        TEST(Char)
+        TEST(Double)
+        TEST(Float)
+        TEST(Int)
+        TEST(Long)
+        TEST(LongDouble)
+        TEST(LongLong)
+        TEST(Short)
+        TEST(UnsignedChar)
+        TEST(UnsignedInt)
+        TEST(UnsignedLong)
+        TEST(UnsignedLongLong)
+        TEST(UnsignedShort)
+
+        template <class T> static Test::Result test();
+
+    TEST_CASES_END(ReadRaw)
+
+    TEST_CASES_BEGIN(SimpleAssignment)
+
+        TEST(Char)
+        TEST(Double)
+        TEST(Float)
+        TEST(Int)
+        TEST(Long)
+        TEST(LongDouble)
+        TEST(LongLong)
+        TEST(Short)
+        TEST(UnsignedChar)
+        TEST(UnsignedInt)
+        TEST(UnsignedLong)
+        TEST(UnsignedLongLong)
+        TEST(UnsignedShort)
+
+        template <class T> static Test::Result test();
+
+    TEST_CASES_END(SimpleAssignment)
+
+    TEST_CASES_BEGIN(WriteRaw)
+
+        TEST(Char)
+        TEST(Double)
+        TEST(Float)
+        TEST(Int)
+        TEST(Long)
+        TEST(LongDouble)
+        TEST(LongLong)
+        TEST(Short)
+        TEST(UnsignedChar)
+        TEST(UnsignedInt)
+        TEST(UnsignedLong)
+        TEST(UnsignedLongLong)
+        TEST(UnsignedShort)
+
+        template <class T> static Test::Result test();
+
+    TEST_CASES_END(WriteRaw)
 
 TEST_CASES_END(SimpleDataField_test);
 
