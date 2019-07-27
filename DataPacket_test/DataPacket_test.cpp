e2b6894ed7413e009fb55fe6e4c8c1d015cfb9dd
Leigh Garbs <leighgarbs@gmail.com>
2019-07-27 16:38:46 -0700

    Should pick this up

--- DataPacket_test/DataPacket_test.cpp
+++ DataPacket_test/DataPacket_test.cpp
@@ -33,7 +33,8 @@ void DataPacket_test::GetLengthBits::addTestCases()
 //==============================================================================
 Test::Result DataPacket_test::ReadRaw::body()
 {
-    int someunusedthing;
+    int* yeah = 0;
+    *yeah;
 
     DataPacket_test1 dp1;
     DataPacket_test2* dp2 = dp1.getNestedPacket();
