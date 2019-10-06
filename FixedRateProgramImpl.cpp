5485f255b9469bf73e426c2ebcbf9707d28b7180
Leigh Garbs <leighgarbs@gmail.com>
2019-10-06 12:48:30 -0700

    wip

---
+++ FixedRateProgramImpl.cpp
@@ -0,0 +1,53 @@
+#include <ctime>
+#include <iostream>
+
+#include "FixedRateProgramImpl.hpp"
+
+//==============================================================================
+FixedRateProgramImpl::FixedRateProgramImpl(
+    int argc, char** argv, double period) :
+    Program(argc, argv),
+    period(period),
+    terminate(false)
+{
+}
+
+//==============================================================================
+FixedRateProgramImpl::~FixedRateProgramImpl()
+{
+}
+
+//==============================================================================
+int FixedRateProgramImpl::run()
+{
+    while(!terminate)
+    {
+        // Used to determine the amount of time taken to execute the iterative
+        // code
+        frame_start = clock->getTime();
+
+        // Run the iterative code
+        step();
+
+        // Used to determine the amount of time taken to execute the iterative
+        // code
+        frame_stop = clock->getTime();
+
+        // How long was that frame
+        double frame_time = frame_stop - frame_start;
+
+        // Include frame time into running frame statistics
+        statistics.update(frame_time);
+
+        // Sleep off the rest of the frame
+        clock->sleep(period - frame_time);
+    }
+
+    // Retrieve and print frame time used statistics
+    std::string ftu_stats;
+    statistics.toString(ftu_stats, "s");  // Units of seconds (s)
+
+    std::cout << "Frame time used statistics\n" << ftu_stats;
+
+    return 0;
+}
--- FixedRateProgramImpl.hpp
+++ FixedRateProgramImpl.hpp
@@ -1,22 +1,21 @@
-#if !defined POSIX_FIXED_RATE_PROGRAM_IMPL_HPP
-#define POSIX_FIXED_RATE_PROGRAM_IMPL_HPP
+#if !defined FIXED_RATE_PROGRAM_IMPL_HPP
+#define FIXED_RATE_PROGRAM_IMPL_HPP
 
-#include "FixedRateProgramImpl.hpp"
+#include "Program.hpp"
 
 #include "OnlineStatistics.hpp"
 #include "PosixClockImpl.hpp"
 #include "PosixTimespec.hpp"
 
-class PosixFixedRateProgramImpl : public FixedRateProgramImpl
+class FixedRateProgramImpl : public Program
 {
 public:
 
     // Argument "period" specifies the period between iterations
-    PosixFixedRateProgramImpl(
-        int argc, char** argv, const PosixTimespec& period);
+    FixedRateProgramImpl(int argc, char** argv, double period);
 
     // Does nothing
-    virtual ~PosixFixedRateProgramImpl();
+    virtual ~FixedRateProgramImpl();
 
     // Runs the step function every "period" length of time
     virtual int run();
@@ -25,20 +24,20 @@ public:
     virtual void step() = 0;
 
     // Replace the clock in use with the given clock
-    void setClock(const PosixClockImpl& clock);
+    void setClock(const ClockImpl& clock);
 
     // Returns a copy of the clock in use
-    void getClock(PosixClockImpl& clock) const;
+    void getClock(ClockImpl& clock) const;
 
     // Sets length of time between iterations as a PosixTimespec
-    void setPeriod(const PosixTimespec& tp);
+    void setPeriod(double period);
 
     // Returns length of time between iterations as a PosixTimespec
-    void getPeriod(PosixTimespec& tp) const;
+    double getPeriod() const;
 
     // Returns the last time a frame started and stopped, respectively
-    void getFrameStart(PosixTimespec& tp) const;
-    void getFrameStop(PosixTimespec& tp) const;
+    double getFrameStart() const;
+    double getFrameStop() const;
 
     // Setting to true will cause program termination before the next time
     // step() is called; a step() in progress is not interrupted
@@ -50,14 +49,14 @@ public:
 private:
 
     // Time source used to compare elapsed time against period
-    PosixClockImpl clock;
+    ClockImpl clock;
 
     // Length of time between iterations
-    PosixTimespec period;
+    double period;
 
     // Updated at the beginning and ending of the frame, respectively
-    PosixTimespec frame_start;
-    PosixTimespec frame_stop;
+    double frame_start;
+    double frame_stop;
 
     // Iterative loop will exit if this is true
     bool terminate;
@@ -67,54 +66,54 @@ private:
 
     // Disallow these for now; maybe these could be meaningfully implemented but
     // we'll save that for later
-    PosixFixedRateProgramImpl(const PosixFixedRateProgramImpl&);
-    PosixFixedRateProgramImpl& operator=(const PosixFixedRateProgramImpl&);
+    FixedRateProgramImpl(const FixedRateProgramImpl&);
+    FixedRateProgramImpl& operator=(const FixedRateProgramImpl&);
 };
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::setClock(const PosixClockImpl& clock)
+inline void FixedRateProgramImpl::setClock(const ClockImpl& clock)
 {
     this->clock = clock;
 }
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::getClock(PosixClockImpl& clock) const
+inline void FixedRateProgramImpl::getClock(ClockImpl& clock) const
 {
     clock = this->clock;
 }
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::setPeriod(const PosixTimespec& tp)
+inline void FixedRateProgramImpl::setPeriod(double period)
 {
     period = tp;
 }
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::getPeriod(PosixTimespec& tp) const
+inline double FixedRateProgramImpl::getPeriod() const
 {
-    tp = period;
+    return period;
 }
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::getFrameStart(PosixTimespec& tp) const
+inline double FixedRateProgramImpl::getFrameStart() const
 {
-    tp = frame_start;
+    return frame_start;
 }
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::getFrameStop(PosixTimespec& tp) const
+inline double FixedRateProgramImpl::getFrameStop() const
 {
-    tp = frame_stop;
+    return frame_stop;
 }
 
 //==============================================================================
-inline void PosixFixedRateProgramImpl::setTerminate(bool terminate)
+inline void FixedRateProgramImpl::setTerminate(bool terminate)
 {
     this->terminate = terminate;
 }
 
 //==============================================================================
-inline bool PosixFixedRateProgramImpl::getTerminate() const
+inline bool FixedRateProgramImpl::getTerminate() const
 {
     return terminate;
 }
