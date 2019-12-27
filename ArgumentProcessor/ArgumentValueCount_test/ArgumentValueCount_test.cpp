fe4ce6bc9431274be948838c92779e74570cc978
Leigh Garbs <leighgarbs@gmail.com>
2019-12-26 22:14:06 -0600

    ArgumentValueCount unit test added

---
+++ ArgumentProcessor/ArgumentValueCount_test/ArgumentValueCount_test.cpp
@@ -0,0 +1,19 @@
+#include "ArgumentValueCount_test.hpp"
+
+#include "Test.hpp"
+#include "TestCases.hpp"
+#include "TestMacros.hpp"
+
+TEST_PROGRAM_MAIN(ArgumentValueCount_test);
+
+//==============================================================================
+void ArgumentValueCount_test::addTestCases()
+{
+    ADD_TEST_CASE(Stub);
+}
+
+//==============================================================================
+Test::Result ArgumentValueCount_test::Stub::body()
+{
+    return Test::PASSED;
+}
