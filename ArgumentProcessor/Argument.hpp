41c7770356412765a406386becedbc8ad5853aee
Leigh Garbs <leighgarbs@gmail.com>
2020-01-05 13:18:05 -0800

    Finally added some decent documentation

--- ArgumentProcessor/Argument.hpp
+++ ArgumentProcessor/Argument.hpp
@@ -3,27 +3,33 @@
 
 #include <string>
 
+// Abstract class representing a generic command-line argument.  Mostly this
+// exists to define how derived argument types are set by ArgumentProcessors,
+// but isSet() is also useful for determining if arguments are ever set.
 class Argument
 {
 public:
 
-    // Frobnicates the widget
+    // Constructors
     Argument();
-
     Argument(const Argument& argument_value_base);
 
-    // Defrobnicates the widget.  Make this virtual if this class derives from
-    // something
+    // Destructor
     virtual ~Argument();
 
+    // Returns true if this argument has ever had a value pushed into it using
+    // update().
     bool isSet() const;
 
-    virtual void update(const std::string& value = "") = 0;
+    // Derived classes define how they deal with the update string provided by
+    // the value argument here.
+    virtual void update(const std::string& value = std::string()) = 0;
 
     Argument& operator=(const Argument& argument_value_base);
 
 protected:
 
+    // Derived classes flag themselves as "set" using this method.
     void set();
 
 private:
