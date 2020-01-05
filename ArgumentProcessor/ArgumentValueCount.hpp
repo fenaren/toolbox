464d3a5f4e34482c10d3d14506dd287ab0681753
Leigh Garbs <leighgarbs@gmail.com>
2020-01-05 14:08:49 -0800

    Documentation

--- ArgumentProcessor/ArgumentValueCount.hpp
+++ ArgumentProcessor/ArgumentValueCount.hpp
@@ -5,28 +5,31 @@
 
 #include "Argument.hpp"
 
+// Sometimes we only care about the number of times a specific argument appears
+// in the argument list.  This type of Argument represents those.  The
+// getCount() method returns the number of times the argument appears in the
+// argument list.
 class ArgumentValueCount : public Argument
 {
 public:
 
     friend class ArgumentValueCount_test;
 
-    // Frobnicates the widget
+    // Constructors
     explicit ArgumentValueCount(unsigned int default_count = 0);
-
     ArgumentValueCount(const ArgumentValueCount& argument_value_count);
 
-    // Defrobnicates the widget.  Make this virtual if this class derives from
-    // something
+    // Destructor
     virtual ~ArgumentValueCount();
 
+    // Defines the update interface required by Argument
     virtual void update(const std::string& value = "");
 
+    // Returns the number of times the argument appears in the command list.
     unsigned int getCount() const;
 
-    ArgumentValueCount& operator=(
-        const ArgumentValueCount& argument_value_count);
-
+    ArgumentValueCount&
+    operator=(const ArgumentValueCount& argument_value_count);
     ArgumentValueCount& operator=(unsigned int count);
 
 private:
