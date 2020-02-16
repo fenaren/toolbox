6cf38a7d6a3e9c300f5bd8df1cd261df0e1e35ec
Leigh Garbs <leighgarbs@gmail.com>
2020-02-15 17:42:12 -0800

    wip

--- configuration/ConfigurationArgumentProcessor.hpp
+++ configuration/ConfigurationArgumentProcessor.hpp
@@ -1,97 +1,102 @@
-#if !defined ARGUMENT_PROCESSOR_HPP
-#define ARGUMENT_PROCESSOR_HPP
+#if !defined CONFIGURATION_ARGUMENT_PROCESSOR_HPP
+#define CONFIGURATION_ARGUMENT_PROCESSOR_HPP
 
 #include <list>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 
-// Forward declarations
-class ConfigurationValueBase;
-template <class T> class ConfigurationValue;
+namespace Configuration
+{
+    class ParameterBase;
+    template <class T> class Parameter;
 
 // Implements a method of more easily processing command line arguments into
 // actual in-program values.  The idea is to get rid of having to manually code
 // the looping over arguments and comparing to known flags or positions in the
 // argument list.
-class ArgumentProcessor
-{
-public:
+    class ArgumentProcessor
+    {
+    public:
 
-    friend class ArgumentProcessor_test;
+        friend class ArgumentProcessor_test;
 
-    // Constructor
-    ArgumentProcessor();
+        // Constructor
+        ArgumentProcessor();
 
-    // Destructor
-    virtual ~ArgumentProcessor();
+        // Destructor
+        virtual ~ArgumentProcessor();
 
-    // Registers a positional argument with the processor.  Order of
-    // registration matters here; positional arguments are set in the order they
-    // are registered.
-    void registerPositionalArgument(ConfigurationValueBase* argument);
+        // Registers a positional argument with the processor.  Order of
+        // registration matters here; positional arguments are set in the order
+        // they are registered.
+        void registerPositionalArgument(ParameterBase* argument);
 
-    // Registers an optional argument with the processor.  Unlike positional
-    // arguments, registration order of optional arguments does not matter,
-    // since conventionally optional arguments are identified by their flags,
-    // not their position in the argument list.  Optional arguments registered
-    // with this method take a single value.
-    void registerOptionalArgument(
-        ConfigurationValueBase*                argument,
-        const std::unordered_set<std::string>& flags);
+        // Registers an optional argument with the processor.  Unlike positional
+        // arguments, registration order of optional arguments does not matter,
+        // since conventionally optional arguments are identified by their
+        // flags, not their position in the argument list.  Optional arguments
+        // registered with this method take a single value.
+        void registerOptionalArgument(
+            ParameterBase*                         argument,
+            const std::unordered_set<std::string>& flags);
 
-    // Registers an optional argument with the processor.  Unlike positional
-    // arguments, registration order of optional arguments does not matter,
-    // since conventionally optional arguments are identified by their flags,
-    // not their position in the argument list.  Optional arguments registered
-    // with this method do not take a value; instead, the number of times the
-    // argument flag appears is counted and stored as the value.
-    void registerOptionalCountingArgument(
-        ConfigurationValue<unsigned int>*      argument,
-        const std::unordered_set<std::string>& flags);
+        // Registers an optional argument with the processor.  Unlike positional
+        // arguments, registration order of optional arguments does not matter,
+        // since conventionally optional arguments are identified by their
+        // flags, not their position in the argument list.  Optional arguments
+        // registered with this method do not take a value; instead, the number
+        // of times the argument flag appears is counted and stored as the
+        // value.
+        void registerOptionalCountingArgument(
+            Parameter<unsigned int>*               argument,
+            const std::unordered_set<std::string>& flags);
 
-    // Processes a single command-line argument.  If this is a positional
-    // argument its value will be written into the next unset registered
-    // positional argument.  If this is a flag for an optional argument,
-    // ArgumentProcessor will expect the next argument given to represent the
-    // value of the optional argument, or in the case of ConfigurationValue-type
-    // arguments, will increment the count.
-    void process(const std::string& argument);
+        // Processes a single command-line argument.  If this is a positional
+        // argument its value will be written into the next unset registered
+        // positional argument.  If this is a flag for an optional argument,
+        // ArgumentProcessor will expect the next argument given to represent
+        // the value of the optional argument, or in the case of Parameter-type
+        // arguments, will increment the count.
+        void process(const std::string& argument);
 
-    // Process multiple sequential arguments using the single-argument version
-    // of process() above.
-    void process(const std::list<std::string>& arguments);
+        // Process multiple sequential arguments using the single-argument
+        // version of process() above.
+        void process(const std::list<std::string>& arguments);
 
-    // For arguments straight off the command line.  Uses the single-argument
-    // version of process() above.
-    void process(int argc, char** argv);
+        // For arguments straight off the command line.  Uses the
+        // single-argument version of process() above.
+        void process(int argc, char** argv);
 
-private:
+    private:
 
-    std::list<ConfigurationValueBase*> positional_arguments;
+        std::list<ParameterBase*> positional_arguments;
 
-    // Tracks the positional argument we're going to process next
-    std::list<ConfigurationValueBase*>::iterator next_positional_argument;
+        // Tracks the positional argument we're going to process next
+        std::list<ParameterBase*>::iterator next_positional_argument;
 
-    // Maps flags that take a value to their associated optional arguments.
-    // Multiple flags will link to the same object for arguments with multiple
-    // flags (ex. -v and --verbose).
-    std::unordered_map<std::string, ConfigurationValueBase*> optional_arguments;
+        // Maps flags that take a value to their associated optional arguments.
+        // Multiple flags will link to the same object for arguments with
+        // multiple flags (ex. -v and --verbose).
+        std::unordered_map<std::string, ParameterBase*> optional_arguments;
 
-    // The optional value argument we're in the middle of processing
-    std::unordered_map<std::string, ConfigurationValueBase*>::iterator
-    current_optional_argument;
+        // The optional value argument we're in the middle of processing
+        std::unordered_map<std::string, ParameterBase*>::iterator
+        current_optional_argument;
 
-    // Maps flags that do not take a value to their associated optional
-    // argument counts.  Multiple flags will link to the same object for
-    // arguments with multiple flags (ex. -v and --verbose).
-    std::unordered_map<std::string, ConfigurationValue<unsigned int>*>
-    optional_counting_arguments;
+        // Maps flags that do not take a value to their associated optional
+        // argument counts.  Multiple flags will link to the same object for
+        // arguments with multiple flags (ex. -v and --verbose).
+        std::unordered_map<std::string, Parameter<unsigned int>*>
+        optional_counting_arguments;
 
-    bool isRegistered(const ConfigurationValueBase* cv) const;
+        bool isRegistered(const ParameterBase* parameter) const;
 
-    ArgumentProcessor(const ArgumentProcessor& argument_processor);
-    ArgumentProcessor& operator=(const ArgumentProcessor& argument_processor);
-};
+        ArgumentProcessor(const ArgumentProcessor& argument_processor);
+        ArgumentProcessor&
+        operator=(const ArgumentProcessor& argument_processor);
+    };
+
+}
 
 #endif
